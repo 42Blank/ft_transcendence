212714bc2d4fb6d987cb2b0e2c16a1eb40603d86
huni <petercha2000@gmail.com>
2023-01-27 21:26:34 +0900

    feat: ConfigModule 추가함

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as morgan from 'morgan';
@@ -5,15 +6,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   morgan.token('body', req => JSON.stringify((req as unknown as { body: string }).body));
   app.use(morgan(':method :url :status :body > :res[content-length] bytes :response-time ms'));
 
   const config = new DocumentBuilder()
-    .setTitle(`Transcendence API - ${process.env.PROFILE || 'dev'} (${process.env.VERSION || 'local'})`)
+    .setTitle(
+      `Transcendence API - ${configService.get('PROFILE') || 'dev'} (${configService.get('VERSION') || 'local'})`,
+    )
     .setDescription(`ft_transcendence api`)
     .setVersion('0.1')
-    .addCookieAuth(process.env.ACCESS_TOKEN_KEY)
+    .addCookieAuth(configService.get('ACCESS_TOKEN_KEY'))
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document, {
@@ -21,13 +25,13 @@ async function bootstrap() {
     customCss: '.swagger-ui .topbar { display: none }',
   });
 
-  const originList = process.env.ORIGIN_LIST || '';
-  const originRegex = process.env.ORIGIN_REGEX ? new RegExp(process.env.ORIGIN_REGEX) : '';
+  const originList = configService.get<string>('ORIGIN_LIST') || '';
+  const originRegex = configService.get('ORIGIN_REGEX') ? new RegExp(configService.get('ORIGIN_REGEX')) : '';
   app.enableCors({
     origin: [...originList.split(',').map(item => item.trim()), originRegex],
     credentials: true,
   });
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
