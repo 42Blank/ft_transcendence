92306941a7311baf1831d3c7d0d470b18dbe48bc
chichoon <chichoon.choi@gmail.com>
2023-02-06 17:53:32 +0900

    chore: :rotating_light: 린터 영차가 추가해놨던거랑 내가 만들던거 합침
    삼단합체

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,10 +1,19 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
   extends: [
     'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
     'airbnb/hooks',
     'react-app',
     'prettier',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:prettier/recommended',
   ],
   settings: {
@@ -20,28 +29,23 @@ module.exports = {
     },
   },
   rules: {
+    'linebreak-style': 'off',
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off', // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
+    'react/prop-types': 'off',
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    '@typescript-eslint/no-shadow': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-use-before-define': 'off',
+
     'react/react-in-jsx-scope': 'off',
     'lines-between-class-members': 'off',
-    'import/no-unresolved': 'off',
     'prefer-template': 'warn',
-    'import/prefer-default-export': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: ['.storybook/**', 'src/stories/**'],
-      },
-    ],
   },
 };
