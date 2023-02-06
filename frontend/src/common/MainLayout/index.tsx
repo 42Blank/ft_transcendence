4a83879cd70329427467ffe5698c500a86a2ebd0
chichoon <chichoon.choi@gmail.com>
2023-02-06 18:08:33 +0900

    feat: :sparkles: 루트 경로에 진입할 경우 chat으로 리디렉션

--- frontend/src/common/MainLayout/index.tsx
+++ frontend/src/common/MainLayout/index.tsx
@@ -1,12 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, useNavigate, Outlet } from 'react-router-dom';
 
 import { NavBar } from './NavBar';
 
 export const MainLayout = () => {
+  const { pathname } = useLocation();
+  const nav = useNavigate();
+
+  useEffect(() => {
+    if (pathname === '/') nav('/chat');
+  }, [pathname, nav]);
+
   return (
     <>
       <NavBar />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 };
