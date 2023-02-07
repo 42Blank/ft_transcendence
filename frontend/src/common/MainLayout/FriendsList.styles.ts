cbd697b689064795a09118e4f3469bf1e5bbbffc
chichoon <chichoon.choi@gmail.com>
2023-02-07 22:36:31 +0900

    feat: :sparkles: 친구목록 컴포넌트 추가
    메인 레이아웃에 붙임

--- frontend/src/common/MainLayout/FriendsList.styles.ts
+++ frontend/src/common/MainLayout/FriendsList.styles.ts
@@ -1,5 +1,12 @@
 import { css } from '@emotion/css';
 
-export const friendsListStyles = css({
+export const friendsListStyle = css({
   width: 250,
+});
+
+export const friendsListTitleStyle = css({
+  width: '100%',
+  textAlign: 'center',
+  padding: '10px 0',
+  marginBottom: 10,
 });
