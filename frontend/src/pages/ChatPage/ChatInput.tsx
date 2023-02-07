1d81510f420e089742cb4fba8b741523209f63fe
chichoon <chichoon.choi@gmail.com>
2023-02-08 05:45:40 +0900

    feat: :sparkles: 전송 버튼 클릭하면 전역 상태값 변경

--- frontend/src/pages/ChatPage/ChatInput.tsx
+++ frontend/src/pages/ChatPage/ChatInput.tsx
@@ -1,13 +1,22 @@
+import { useRef } from 'react';
+import { useSetRecoilState } from 'recoil';
+
+import { newMessageState } from 'store';
+
 import { chatInputStyle } from './ChatInput.styles';
 
 export const ChatInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const setNewMessage = useSetRecoilState(newMessageState);
+
   function handleClickButton() {
-    console.log('바보들아');
+    setNewMessage(inputRef.current.value); // TODO: socket hook 에서 emit
+    inputRef.current.value = '';
   }
 
   return (
     <div className={chatInputStyle}>
-      <input type="text" placeholder="메시지를 입력하세요..." />
+      <input type="text" placeholder="메시지를 입력하세요..." ref={inputRef} />
       <button type="button" onClick={handleClickButton}>
         {'>'}
       </button>
