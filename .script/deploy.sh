7f1addcd3d26fdb95a538d8a54a8ed03161ccf63
huni <petercha2000@gmail.com>
2023-02-08 03:40:28 +0900

    feat: db port 도 바꾼다

--- .script/deploy.sh
+++ .script/deploy.sh
@@ -13,6 +13,7 @@ yes | cp backend/.env.prod backend/.env
 docker-compose up --build -d
 
 sed -i 's/8081:/8083:/g' docker-compose.yml
+sed -i 's/5432:/5433:/g' docker-compose.yml
 sed -i 's/FT_CALLBACK_URL=.*/FT_CALLBACK_URL=https:\/\/localhost:3000\/login\/callback/' ./backend/.env
 
 docker-compose -p for_local up -d backend
