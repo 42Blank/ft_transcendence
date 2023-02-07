a34939f90b737ddb2d2f2e2f044d4f0bf63aec48
chichoon <chichoon.choi@gmail.com>
2023-02-08 00:31:45 +0900

    feat: :sparkles: 로그인 한 상태에선 로그인 페이지 접근 불가

--- frontend/src/hooks/useCheckLogin.ts
+++ frontend/src/hooks/useCheckLogin.ts
@@ -4,6 +4,7 @@ import { useRecoilState } from 'recoil';
 
 import { getCurrentUserInfo } from 'services';
 import { userState } from 'store';
+import { ROUTE } from 'common/constants';
 import { UserInfoType } from 'types/user';
 
 export function useCheckLogin() {
@@ -12,11 +13,17 @@ export function useCheckLogin() {
   const nav = useNavigate();
 
   useEffect(() => {
-    if (userInfo.id >= 0) return;
+    if (userInfo.id >= 0) {
+      if (pathname === ROUTE.LOGIN) nav(ROUTE.CHAT);
+      return;
+    }
     getCurrentUserInfo()
       .then((res: void | UserInfoType) => {
         if (!res) throw Error();
         setUserInfo(res);
+      })
+      .then(() => {
+        if (pathname === ROUTE.LOGIN) nav(ROUTE.CHAT);
       })
       .catch(() => {
         nav('/login');
