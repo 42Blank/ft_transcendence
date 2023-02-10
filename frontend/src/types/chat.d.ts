0273b6dffc931542906779f12e3a67aa33b127a9
chichoon <chichoon.choi@gmail.com>
2023-02-10 19:21:38 +0900

    feat: :label: 채팅방 정보 담은 타입 추가

--- frontend/src/types/chat.d.ts
+++ frontend/src/types/chat.d.ts
@@ -1,6 +1,19 @@
+import { UserInfoType } from './user';
+
 export interface ChatDataType {
   nickname: string;
   avatar: string;
   message: string;
   timestamp: string;
 } // TODO: 바뀔 수도 있음, 백엔드와 상의 필요
+
+interface ChatUserInfoType extends UserInfoType {
+  isOperator: boolean;
+}
+
+export interface ChatRoomInfoType {
+  roomTitle: string;
+  isPrivate: boolean;
+  password?: string;
+  users: ChatUserInfoType[];
+}
