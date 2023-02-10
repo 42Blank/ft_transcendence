5195bad5245b4fd651ac7a2cb82d66cbfd63d6d0
huni <petercha2000@gmail.com>
2023-02-11 01:48:10 +0900

    fix: jwt payload 에 Intraid 도 추가함

--- backend/src/api/auth/jwt-auth/jwt-auth.module.ts
+++ backend/src/api/auth/jwt-auth/jwt-auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../../../common/database/entities/user.entity';
 import { JwtAuthStrategy } from './jwt-auth.strategy';
 
 @Module({
-  imports: [PassportModule],
+  imports: [PassportModule, TypeOrmModule.forFeature([User])],
   providers: [JwtAuthStrategy],
 })
 export class JwtAuthModule {}
