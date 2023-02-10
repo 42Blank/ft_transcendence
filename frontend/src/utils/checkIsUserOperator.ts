1a065348edb46fb393f2c6cdf2ad8a271bd6ef41
chichoon <chichoon.choi@gmail.com>
2023-02-11 03:11:11 +0900

    refactor: :recycle: checkIsUserOperator 인자로 유저 대신 id 받게 수정

--- frontend/src/utils/checkIsUserOperator.ts
+++ frontend/src/utils/checkIsUserOperator.ts
@@ -1,6 +1,5 @@
 import { ChatUserInfoType } from 'types/chat';
-import { UserInfoType } from 'types/user';
 
-export function checkIsUserOperator(users: ChatUserInfoType[], currentUser: UserInfoType) {
-  return users.some(({ id, isOperator }) => id === currentUser.id && isOperator);
+export function checkIsUserOperator(users: ChatUserInfoType[], currentUserId: number) {
+  return users.some(({ id, isOperator }) => id === currentUserId && isOperator);
 }
