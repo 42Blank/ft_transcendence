c561c189619059b3c5d1f82fa941e84f00af0dc8
chichoon <chichoon.choi@gmail.com>
2023-02-11 02:14:32 +0900

    feat: :sparkles: 햄버거 버튼 누르면 채팅방 추가정보 나오게 함

--- frontend/src/pages/ChatPage/index.tsx
+++ frontend/src/pages/ChatPage/index.tsx
@@ -1,10 +1,8 @@
-import { HamburgerIcon } from 'assets';
-import { useRecoilValue } from 'recoil';
-// import { useParams } from 'react-router-dom';
-
-import { currentUserState } from 'store';
+import { useGetCurrentUser, useModal } from 'hooks';
 import { ChatDataType } from 'types/chat';
+import { HamburgerIcon } from 'assets';
 import { ChatElement } from './ChatElement';
+import { ChatInfoModal } from './ChatInfoModal';
 import { ChatInput } from './ChatInput';
 
 import { chatPageListWrapperStyle, chatPageTitleStyle, chatPageWrapperStyle } from './ChatPage.styles';
@@ -69,31 +67,72 @@ const DUMMY_CHAT: ChatDataType[] = [
   },
 ];
 
+const DUMMY_CHAT_INFO = {
+  roomTitle: 'ycha 바보',
+  isPrivate: true,
+  users: [
+    {
+      id: 1,
+      nickname: '자송 the 엄청긴닉네임소유자',
+      intraId: '11111',
+      avatar: 'https://cdn.intra.42.fr/users/b3af07505013b8500276523f65b49ff1/jasong.JPG',
+      point: 0,
+      createdAt: '1970-01-01T00:00:00.000Z',
+      updatedAt: '1970-01-01T00:00:00.000Z',
+      isOperator: true,
+    },
+    {
+      id: 5,
+      nickname: '지최',
+      intraId: '55555',
+      avatar: 'https://cdn.intra.42.fr/users/b9c44cf9ae13ffec04381638c0a2f204/jiychoi.jpg',
+      point: 0,
+      createdAt: '1970-01-01T00:00:00.000Z',
+      updatedAt: '1970-01-01T00:00:00.000Z',
+      isOperator: true,
+    },
+    {
+      id: 4,
+      nickname: '영차',
+      intraId: '44444',
+      avatar: 'https://cdn.intra.42.fr/users/6038c103da3bb9180a072f8154e6b428/ycha.jpg',
+      point: 0,
+      createdAt: '1970-01-01T00:00:00.000Z',
+      updatedAt: '1970-01-01T00:00:00.000Z',
+      isOperator: false,
+    },
+  ],
+};
+
 export const ChatPage = () => {
   // const { id } = useParams();
-  const { id: currentUserID } = useRecoilValue(currentUserState);
+  const { id: currentUserID } = useGetCurrentUser();
+  const { modalRef, isShown, handleOpenModal, handleCloseModal } = useModal();
   // const currentChatData = useRecoilValue(currentChatDataState);
 
   return (
-    <main className={chatPageWrapperStyle}>
-      <header className={chatPageTitleStyle}>
-        <span>ycha 바보</span>
-        <button type="button">
-          <HamburgerIcon /> {/* TODO: 더 잘 어울리는 아이콘 있으면 그걸로 바꿀 예정 */}
-        </button>
-      </header>
-      <ul className={chatPageListWrapperStyle}>
-        {DUMMY_CHAT.map(({ user, message, timestamp }, index) => (
-          <ChatElement
-            key={`${index}-${user.nickname}`}
-            user={user}
-            message={message}
-            timestamp={timestamp}
-            isMine={currentUserID === user.id}
-          />
-        ))}
-      </ul>
-      <ChatInput />
-    </main>
+    <>
+      <main className={chatPageWrapperStyle}>
+        <header className={chatPageTitleStyle}>
+          <span>{DUMMY_CHAT_INFO.roomTitle}</span>
+          <button type="button" onClick={handleOpenModal}>
+            <HamburgerIcon /> {/* TODO: 더 잘 어울리는 아이콘 있으면 그걸로 바꿀 예정 */}
+          </button>
+        </header>
+        <ul className={chatPageListWrapperStyle}>
+          {DUMMY_CHAT.map(({ user, message, timestamp }, index) => (
+            <ChatElement
+              key={`${index}-${user.nickname}`}
+              user={user}
+              message={message}
+              timestamp={timestamp}
+              isMine={currentUserID === user.id}
+            />
+          ))}
+        </ul>
+        <ChatInput />
+      </main>
+      {isShown && <ChatInfoModal modalRef={modalRef} chatInfo={DUMMY_CHAT_INFO} onClickClose={handleCloseModal} />}
+    </>
   );
 };
