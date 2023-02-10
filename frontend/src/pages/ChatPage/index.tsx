dc413fb6eb424fa645c15cef4903968ef8556936
chichoon <chichoon.choi@gmail.com>
2023-02-10 19:21:56 +0900

    chore: :sparkles: 햄버거 아이콘 추가 (임시), 목데이터 삭제

--- frontend/src/pages/ChatPage/index.tsx
+++ frontend/src/pages/ChatPage/index.tsx
@@ -1,93 +1,25 @@
+import { HamburgerIcon } from 'assets';
 import { useRecoilValue } from 'recoil';
 // import { useParams } from 'react-router-dom';
 
-import { currentChatDataState, userState } from 'store';
+import { currentChatDataState, currentUserState } from 'store';
 import { ChatElement } from './ChatElement';
 import { ChatInput } from './ChatInput';
 
 import { chatPageListWrapperStyle, chatPageTitleStyle, chatPageWrapperStyle } from './ChatPage.styles';
 
-// const DUMMY_CHAT = [
-//   {
-//     nickname: 'ycha',
-//     avatar: 'https://cdn.intra.42.fr/users/6038c103da3bb9180a072f8154e6b428/ycha.jpg',
-//     message: '이방 뭐임',
-//     timestamp: '1970-01-01T00:00:00.000Z',
-//   },
-//   {
-//     nickname: 'jiychoi',
-//     avatar: 'https://cdn.intra.42.fr/users/b9c44cf9ae13ffec04381638c0a2f204/jiychoi.jpg',
-//     message: 'ㅋㅋ',
-//     timestamp: '1970-01-01T00:01:00.000Z',
-//   },
-//   {
-//     nickname: 'asdasdadasdasdads',
-//     avatar: 'https://san.chosun.com/news/photo/202205/15750_66157_37.jpg',
-//     message:
-//       'ㅋㅎㅋㅎㅋㅎㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅎㅋㅎ엄청긴채팅ㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋ',
-//     timestamp: '1970-01-01T00:02:00.000Z',
-//   },
-//   {
-//     nickname: 'san',
-//     avatar: 'https://san.chosun.com/news/photo/202205/15750_66157_37.jpg',
-//     message:
-//       'ㅋㅎㅋㅎㅋㅎㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅎㅋㅎ엄청긴채팅ㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋ',
-//     timestamp: '1970-01-01T00:02:00.000Z',
-//   },
-//   {
-//     nickname: 'san',
-//     avatar: 'https://san.chosun.com/news/photo/202205/15750_66157_37.jpg',
-//     message:
-//       'ㅋㅎㅋㅎㅋㅎㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅎㅋㅎ엄청긴채팅ㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋ',
-//     timestamp: '1970-01-01T00:02:00.000Z',
-//   },
-//   {
-//     nickname: 'san',
-//     avatar: 'https://san.chosun.com/news/photo/202205/15750_66157_37.jpg',
-//     message:
-//       'ㅋㅎㅋㅎㅋㅎㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅎㅋㅎ엄청긴채팅ㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋ',
-//     timestamp: '1970-01-01T00:02:00.000Z',
-//   },
-//   {
-//     nickname: 'san',
-//     avatar: 'https://san.chosun.com/news/photo/202205/15750_66157_37.jpg',
-//     message:
-//       'ㅋㅎㅋㅎㅋㅎㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅎㅋㅎ엄청긴채팅ㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋ',
-//     timestamp: '1970-01-01T00:02:00.000Z',
-//   },
-//   {
-//     nickname: 'san',
-//     avatar: 'https://san.chosun.com/news/photo/202205/15750_66157_37.jpg',
-//     message:
-//       'ㅋㅎㅋㅎㅋㅎㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅎㅋㅎ엄청긴채팅ㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋ',
-//     timestamp: '1970-01-01T00:02:00.000Z',
-//   },
-//   {
-//     nickname: 'san',
-//     avatar: 'https://san.chosun.com/news/photo/202205/15750_66157_37.jpg',
-//     message:
-//       'ㅋㅎㅋㅎㅋㅎㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅎㅋㅎ엄청긴채팅ㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋ',
-//     timestamp: '1970-01-01T00:02:00.000Z',
-//   },
-//   {
-//     nickname: 'san',
-//     avatar: 'https://san.chosun.com/news/photo/202205/15750_66157_37.jpg',
-//     message:
-//       'ㅋㅎㅋㅎㅋㅎㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅎㅋㅎ엄청긴채팅ㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅋㅎㅎㅋㅎㅋㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎㅋ',
-//     timestamp: '1970-01-01T00:02:00.000Z',
-//   },
-// ];
-
 export const ChatPage = () => {
   // const { id } = useParams();
-  const userInfo = useRecoilValue(userState);
+  const userInfo = useRecoilValue(currentUserState);
   const currentChatData = useRecoilValue(currentChatDataState);
 
   return (
     <main className={chatPageWrapperStyle}>
       <header className={chatPageTitleStyle}>
         <span>ycha 바보</span>
-        <button type="button">V</button> {/* TODO: svg로 변경 */}
+        <button type="button">
+          <HamburgerIcon /> {/* TODO: 더 잘 어울리는 아이콘 있으면 그걸로 바꿀 예정 */}
+        </button>
       </header>
       <ul className={chatPageListWrapperStyle}>
         {currentChatData.map(({ nickname, avatar, message, timestamp }, index) => (
