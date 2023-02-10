136488ec64ec62b48a3d01c17ed3ffd95d04fcea
huni <petercha2000@gmail.com>
2023-02-11 00:38:32 +0900

    feat: admin 추가

--- backend/src/common/adapter/socket-io.adapter.ts
+++ backend/src/common/adapter/socket-io.adapter.ts
@@ -1,5 +1,6 @@
 import { INestApplicationContext } from '@nestjs/common';
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import { instrument } from '@socket.io/admin-ui';
 import { Server, ServerOptions } from 'socket.io';
 
 export class SocketIOAdapter extends IoAdapter {
@@ -22,44 +23,13 @@ export class SocketIOAdapter extends IoAdapter {
       },
     };
 
-    // const jwtService = this.app.get(JwtService);
-
     const server: Server = super.createIOServer(port, optionsWithCORS);
 
-    // server.use(createTokenMiddleware(jwtService, this.logger));
+    instrument(server, {
+      auth: false,
+      mode: 'development',
+    });
 
     return server;
   }
-
-  // tokenizePayload(socket: SocketWithAuth, next: NextFunction) {
-  //   // for Postman testing support, fallback to token header
-  //   const token = socket.handshake.auth.token || socket.handshake.headers['token'];
-
-  //   try {
-  //     const payload = jwtService.verify(token);
-  //     socket.userID = payload.sub;
-  //     socket.pollID = payload.pollID;
-  //     socket.name = payload.name;
-  //     next();
-  //   } catch {
-  //     next(new Error('FORBIDDEN'));
-  //   }
-  // }
 }
-
-// const createTokenMiddleware = (jwtService: JwtService, logger: Logger) => (socket: SocketWithAuth, next) => {
-//   // for Postman testing support, fallback to token header
-//   const token = socket.handshake.auth.token || socket.handshake.headers['token'];
-
-//   logger.debug(`Validating auth token before connection: ${token}`);
-
-//   try {
-//     const payload = jwtService.verify(token);
-//     socket.userID = payload.sub;
-//     socket.pollID = payload.pollID;
-//     socket.name = payload.name;
-//     next();
-//   } catch {
-//     next(new Error('FORBIDDEN'));
-//   }
-// };
