9dc2ffce8433f45c8651af7c5538e94f8bb2102e
chichoon <chichoon.choi@gmail.com>
2023-02-11 01:24:37 +0900

    style: :lipstick: edit 아이콘 추가

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,2 +1,3 @@
 export { ReactComponent as HamburgerIcon } from './hamburgerIcon.svg';
 export { ReactComponent as ArrowDownIcon } from './arrowDownIcon.svg';
+export { ReactComponent as EditIcon } from './editIcon.svg';
