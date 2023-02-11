12a728230f8bb785c4ec6017fdfe06e30eb000ff
chichoon <chichoon.choi@gmail.com>
2023-02-11 18:47:04 +0900

    feat: :sparkles: 게임 목록 페이지 채팅 페이지랑 유사하게 꾸미

--- frontend/src/pages/GameListPage/GameListPage.styles.ts
+++ frontend/src/pages/GameListPage/GameListPage.styles.ts
@@ -1,13 +1,50 @@
 import { css } from '@emotion/css';
 
-export const gameListWrapperStyle = css({
-  flex: 1,
-  height: 'calc(100vh - 110px)',
-  overflowX: 'hidden',
-  overflowY: 'scroll',
-  padding: 20,
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridTemplateRows: 'repeat(3, 180px)',
-  gap: 20,
-});
+export const gameListWrapperStyle = css`
+  position: relative;
+  flex: 1;
+  height: calc(100vh - 110px);
+  overflow-x: hidden;
+  overflow-y: scroll;
+  padding: 20px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(3, 180px);
+  gap: 20px;
+`;
+
+export const gameRoomIconStyle = css`
+  position: absolute;
+  z-index: 2;
+  right: 30px;
+  bottom: 30px;
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+  background-color: white; // TODO: 상수화
+  border: 1px solid black; // TODO: 상수화
+
+  & > svg {
+    fill: black; // TODO: 상수화
+  }
+`;
+
+export const newGameModalWrapperStyle = css`
+  height: 30%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const newGameModalHeaderStyle = css`
+  width: calc(100% - 40px);
+  padding: 10px 20px;
+  border-bottom: 1px solid black; // TODO: 색상 상수화
+
+  & > h4 {
+    font-size: 18px;
+    max-width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+`;
--- frontend/src/pages/GameListPage/index.tsx
+++ frontend/src/pages/GameListPage/index.tsx
@@ -1,5 +1,38 @@
-import { gameListWrapperStyle } from './GameListPage.styles';
+import { useState } from 'react';
+
+import { PlusIcon } from 'assets';
+import { Modal } from 'common';
+
+import {
+  gameListWrapperStyle,
+  gameRoomIconStyle,
+  newGameModalHeaderStyle,
+  newGameModalWrapperStyle,
+} from './GameListPage.styles';
 
 export const GameListPage = () => {
-  return <main className={gameListWrapperStyle}>게임</main>;
+  const [isModalShown, setIsModalShown] = useState(false);
+
+  function handleClickButton() {
+    setIsModalShown(true);
+  }
+
+  function handleClickClose() {
+    setIsModalShown(false);
+  }
+
+  return (
+    <main className={gameListWrapperStyle}>
+      <button type="button" onClick={handleClickButton} className={gameRoomIconStyle}>
+        <PlusIcon />
+      </button>
+      {isModalShown && (
+        <Modal onClickClose={handleClickClose} className={newGameModalWrapperStyle}>
+          <header className={newGameModalHeaderStyle}>
+            <h4>새 게임</h4>
+          </header>
+        </Modal>
+      )}
+    </main>
+  );
 };
