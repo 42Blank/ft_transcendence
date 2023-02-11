26b5eb3eb2d51123f1088c67ef13e31ac22484b0
chichoon <chichoon.choi@gmail.com>
2023-02-11 17:49:23 +0900

    feat: :sparkles: 채팅방 생성하는 모달 추가 (아직 내용물은 안만듦)

--- frontend/src/pages/MainPage/MainPage.styles.ts
+++ frontend/src/pages/MainPage/MainPage.styles.ts
@@ -1,13 +1,44 @@
 import { css } from '@emotion/css';
 
-export const MainPageWrapperStyle = css({
-  flex: 1,
-  height: 'calc(100vh - 110px)',
-  overflowX: 'hidden',
-  overflowY: 'scroll',
-  padding: 20,
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridTemplateRows: 'repeat(3, 180px)',
-  gap: 20,
-});
+export const MainPageWrapperStyle = css`
+  position: relative;
+  flex: 1;
+  height: calc(100vh - 110px);
+  overflow-x: hidden;
+  overflow-y: scroll;
+  padding: 20px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 180px);
+  gap: 20px;
+`;
+
+export const chatRoomIconStyle = css`
+  position: absolute;
+  z-index: 2;
+  right: 30px;
+  bottom: 30px;
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+  background-color: white; // TODO: 상수화
+  border: 1px solid black; // TODO: 상수화
+
+  & > svg {
+    fill: black; // TODO: 상수화
+  }
+`;
+
+export const newChatModalHeaderStyle = css`
+  width: calc(100% - 40px);
+  padding: 10px 20px;
+  border-bottom: 1px solid black; // TODO: 색상 상수화
+
+  & > h4 {
+    font-size: 18px;
+    max-width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+`;
--- frontend/src/pages/MainPage/index.tsx
+++ frontend/src/pages/MainPage/index.tsx
@@ -1,8 +1,21 @@
-import { RoomElement } from 'common';
+import { useState } from 'react';
 
-import { MainPageWrapperStyle } from './MainPage.styles';
+import { Modal, RoomElement } from 'common';
+import { PlusIcon } from 'assets';
+
+import { MainPageWrapperStyle, chatRoomIconStyle, newChatModalHeaderStyle } from './MainPage.styles';
 
 export const MainPage = () => {
+  const [isModalShown, setIsModalShown] = useState(false);
+
+  function handleClickButton() {
+    setIsModalShown(true);
+  }
+
+  function handleClickClose() {
+    setIsModalShown(false);
+  }
+
   return (
     <main className={MainPageWrapperStyle}>
       <RoomElement />
@@ -10,6 +23,17 @@ export const MainPage = () => {
       <RoomElement />
       <RoomElement />
       <RoomElement />
+      <button type="button" onClick={handleClickButton} className={chatRoomIconStyle}>
+        <PlusIcon />
+      </button>
+      {isModalShown && (
+        <Modal onClickClose={handleClickClose}>
+          <header className={newChatModalHeaderStyle}>
+            <h4>새 채팅방</h4>
+          </header>
+          <main>이름 공개여부 어쩌구</main>
+        </Modal>
+      )}
     </main>
   );
 };
