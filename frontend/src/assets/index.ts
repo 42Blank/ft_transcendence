257de7e19502b742afbcc6c78fe999b7d8629799
chichoon <chichoon.choi@gmail.com>
2023-02-11 13:38:12 +0900

    style: :lipstick: 뮤트 해제, 어드민 인증 아이콘 추가

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -4,3 +4,6 @@ export { ReactComponent as EditIcon } from './editIcon.svg';
 export { ReactComponent as BanIcon } from './banIcon.svg';
 export { ReactComponent as FightIcon } from './fightIcon.svg';
 export { ReactComponent as MuteIcon } from './muteIcon.svg';
+export { ReactComponent as UnmuteIcon } from './unmuteIcon.svg';
+export { ReactComponent as VerifyIcon } from './verifyIcon.svg';
+export { ReactComponent as VerifiedIcon } from './verifiedIcon.svg';
