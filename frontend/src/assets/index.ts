fc1657f498bd8d3f0efc5bb80b0ae9635f7d9b0e
chichoon <chichoon.choi@gmail.com>
2023-02-11 13:27:57 +0900

    style: :lipstick: 차단, 뮤트, 대결 아이콘 추가

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,3 +1,6 @@
 export { ReactComponent as HamburgerIcon } from './hamburgerIcon.svg';
 export { ReactComponent as ArrowDownIcon } from './arrowDownIcon.svg';
 export { ReactComponent as EditIcon } from './editIcon.svg';
+export { ReactComponent as BanIcon } from './banIcon.svg';
+export { ReactComponent as FightIcon } from './fightIcon.svg';
+export { ReactComponent as MuteIcon } from './muteIcon.svg';
