8025ba2520439663eacbc6384936e451272ca971
chichoon <chichoon.choi@gmail.com>
2023-02-13 15:49:24 +0900

    feat: :sparkles: 채팅방 업데이트 할 때 사용할 타입과 전역 상태값 정의

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,6 +1,7 @@
+export { chatRoomListState } from './chatRoomListState';
+export { currentChatDataState } from './currentChatDataState';
 export { newMessageState } from './newMessageState';
 export { newChatRoomState } from './newChatRoomState';
-export { currentChatDataState } from './currentChatDataState';
-export { chatRoomListState } from './chatRoomListState';
 export { joinChatRoomState } from './joinChatRoomState';
+export { updateChatRoomState } from './updateChatRoomState';
 export { leaveChatRoomState } from './leaveChatRoomState';
--- frontend/src/types/chat.d.ts
+++ frontend/src/types/chat.d.ts
@@ -27,6 +27,13 @@ export interface JoinChatRoomType {
   password?: string;
 }
 
+export interface UpdateChatRoomType {
+  chatRoomId: string;
+  roomTitle: string;
+  isPrivate: boolean;
+  password?: string;
+}
+
 export interface LeaveChatRoomType {
   id: string;
 }
