da3da9e242a7ac425d7af74fea0603f07ea85394
Kang Jisoon <wltnskjs@naver.com>
2023-02-13 15:55:13 +0900

    refactor: :recycle: 신규 이미지 파일 적용 및 코드 정리

--- frontend/src/pages/GamePage/MainScene.tsx
+++ frontend/src/pages/GamePage/MainScene.tsx
@@ -1,36 +1,30 @@
 import Phaser from 'phaser';
 
 export class MainScene extends Phaser.Scene {
-  private pochita: Phaser.Physics.Arcade.Image;
   private ball: Phaser.Physics.Arcade.Image;
   private paddle1: Phaser.Physics.Arcade.Image;
   private paddle2: Phaser.Physics.Arcade.Image;
 
   private key: Phaser.Types.Input.Keyboard.CursorKeys;
-  /* private key: Phaser.Input.Keyboard.Key; */
+  /**
+   * CursorKeys: 지정된 일부 키보드 값만 받을 수 있음
+   * - Up, Down, Right, Left, Shift, SpaceBar
+   * */
 
   constructor() {
     super({ key: 'MainScene', active: true });
   }
   preload() {
-    this.load.image('pochita', '/small_pochita.png');
-    this.load.image('peddal', '/icon.png');
+    this.load.image('ball', '/ball.png');
+    this.load.image('peddal', '/paddle.png');
   }
 
   create() {
-    /* this.physics.world.setBoundsCollision(false, false, false, false); */
-    /*
-    //paddle body가 왜 wolrd랑 충돌 판정 안남?
-    //this.pochita = this.physics.add.image(400, 300, 'pochita');
-    //this.pochita.body.setSize(200, 200);
-    */
-
-    this.ball = this.physics.add.image(100, 100, 'pochita');
+    this.ball = this.physics.add.image(100, 100, 'ball');
     this.ball.setCollideWorldBounds(true);
     this.ball.setBounce(1);
     this.ball.setVelocity(200, 200);
 
-    /* this.paddle1 = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1); */
     this.paddle1 = this.physics.add.image(100, 300, 'peddal');
     this.paddle1.setImmovable(true);
     this.paddle2 = this.physics.add.image(700, 300, 'peddal');
