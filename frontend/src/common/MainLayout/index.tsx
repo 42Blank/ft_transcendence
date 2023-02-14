d8fb7e0f18febb55aa1666a616cdbff81b0e1f48
chichoon <chichoon.choi@gmail.com>
2023-02-15 00:36:12 +0900

    feat: :sparkles: feat: 루트 컴포넌트 추가, 소켓 훅 호출부 해당 컴포넌트로 옮김, 에러 바운더리 추가
    예외처리는 각 컴포넌트에서 진행하지 않고 에러 바운더리로 위임하는 것으로 하겠슴니다

--- frontend/src/common/MainLayout/index.tsx
+++ frontend/src/common/MainLayout/index.tsx
@@ -1,22 +1,13 @@
-import { useEffect, useState } from 'react';
-import { useLocation, useNavigate, Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 
-import { ROUTE } from 'common/constants';
-import { useHandleSocket } from 'hooks';
 import { NavBar } from './NavBar';
 import { FriendsList } from './FriendsList';
 
 import { mainLayoutStyle } from './MainLayout.styles';
 
 export const MainLayout = () => {
-  useHandleSocket(); // TODO: 레이아웃 컴포넌트에서 의존성 줄이기?
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const { pathname } = useLocation();
-  const nav = useNavigate();
-
-  useEffect(() => {
-    if (pathname === ROUTE.ROOT) nav(ROUTE.CHAT);
-  }, [pathname, nav]);
 
   return (
     <>
