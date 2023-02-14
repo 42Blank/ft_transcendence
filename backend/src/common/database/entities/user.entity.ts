7aa8796c1a825b07d1bd0b0066adfd4478c28df7
huni <petercha2000@gmail.com>
2023-02-15 02:33:18 +0900

    feat: friend entity 추가

--- backend/src/common/database/entities/user.entity.ts
+++ backend/src/common/database/entities/user.entity.ts
@@ -1,33 +1,68 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Friend } from './friend.entity';
 
 @Entity('user')
 export class User {
   @ApiProperty({ example: 1, description: '유저 고유번호' })
   @PrimaryGeneratedColumn()
-  id!: number;
+  id: number;
 
   @ApiProperty({ example: '80406', description: '인트라 고유 유저 아이디' })
   @Column({ type: 'varchar', length: 42, nullable: false, unique: true })
-  intraId!: string;
+  intraId: string;
 
   @ApiProperty({ example: 'skyrich', description: '유저 닉네임' })
   @Column({ type: 'varchar', length: 42, nullable: false, unique: true })
-  nickname!: string;
+  nickname: string;
 
   @ApiProperty({ example: 'https://picsum.photos/200', description: '유저 아바타 이미지 url' })
   @Column({ type: 'varchar', length: 100, nullable: false })
-  avatar!: string;
+  avatar: string;
 
   @ApiProperty({ example: 0, description: '유저 포인트' })
   @Column({ type: 'int', nullable: false, default: 0 })
-  point!: number;
+  point: number;
 
   @ApiProperty({ example: new Date(), description: '유저 생성 시간' })
   @CreateDateColumn()
-  createdAt!: Date;
+  createdAt: Date;
 
   @ApiProperty({ example: new Date(), description: '유저 수정 시간' })
   @UpdateDateColumn()
-  updatedAt!: Date;
+  updatedAt: Date;
+
+  // 내가 친구 요청 보낸 목록
+  @OneToMany(() => Friend, friend => friend.recvFriendRequestUser, {
+    createForeignKeyConstraints: false,
+    nullable: true,
+  })
+  sendFriendUsers?: Friend[];
+
+  // 남이 친구해달라고 요청온 목록
+  @OneToMany(() => Friend, friend => friend.sendFriendRequestUser, {
+    createForeignKeyConstraints: false,
+    nullable: true,
+  })
+  recvFriendUsers?: Friend[];
 }
+
+// user 1 : 왓차
+// user 2 : 자송
+
+// 왓차 -> 자송한데 친추했음
+
+// friend {
+//   sendFriendRequestUser : 왓차
+//   recvFriendRequestUser : 자송
+// }
+
+// 왓차 {
+//   내가 요청한 친구 (sendFriendUsers) : 자송
+//   남이 나한테 친구 요청한 사람 (recvFriendUsers) : 없음
+// }
+
+// 자송 {
+//   내가 요청한 친구 (sendFriendUsers) : 없음
+//   남이 나한테 친구 요청한 사람 (recvFriendUsers) : 왓차
+// }
