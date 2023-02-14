c6a0e0529aeb18c15d9feece991da131a4072f58
stripy1026 <inwoo1026@naver.com>
2023-02-14 16:46:49 +0900

    refactor: :sparkles: replace avatar when click sample
    need to add server put avatar api

--- frontend/src/pages/ProfilePage/ProfileModal/EditAvatarModal.tsx
+++ frontend/src/pages/ProfilePage/ProfileModal/EditAvatarModal.tsx
@@ -2,12 +2,17 @@ import { putUserProfile } from 'services';
 import { useRef } from 'react';
 import { useGetCurrentUser } from 'hooks';
 
+import { tmpAvatarStyle } from './tmpAvatarStyle';
+
+import pochi from './pochitaSample.png';
+
 type PropType = {
   onClickClose: () => void;
 };
 
 export const EditAvatarModal = ({ onClickClose }: PropType) => {
   const inputRef = useRef<HTMLInputElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
 
   const { refetch } = useGetCurrentUser();
 
@@ -18,15 +23,31 @@ export const EditAvatarModal = ({ onClickClose }: PropType) => {
     onClickClose();
   }
 
+  function handleClickImage() {
+    putUserProfile({ avatar: imageRef.current.src }).then(() => {
+      refetch();
+    });
+    onClickClose();
+  }
+
   return (
-    <div>
-      <label htmlFor="avatar">Edit Avatar</label>
-      <br />
-      <input type="text" id="avatar" placeholder="new Avatar URL" ref={inputRef} required />
-      <br />
-      <button type="button" onClick={handleSubmitProfile}>
-        submit
-      </button>
-    </div>
+    <>
+      <li>
+        <label htmlFor="avatar">Edit Avatar</label>
+        <br />
+        <input type="text" id="avatar" placeholder="new Avatar URL" ref={inputRef} required />
+        <br />
+        <button type="button" onClick={handleSubmitProfile}>
+          submit
+        </button>
+      </li>
+      <li>
+        <p>Sample Avatar</p>
+        <button type="button" onClick={handleClickImage}>
+          <img className={tmpAvatarStyle} src={pochi} alt="pochi" ref={imageRef} />
+        </button>
+      </li>
+    </>
   );
 };
+// sample avatar further updates are needed
---
+++ frontend/src/pages/ProfilePage/ProfileModal/tmpAvatarStyle.ts
@@ -0,0 +1,7 @@
+import { css } from '@emotion/css';
+
+export const tmpAvatarStyle = css`
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+`;
