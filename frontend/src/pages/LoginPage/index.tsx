f07386ac6db08378f85852ff0f4a1ced36a6c00a
chichoon <chichoon.choi@gmail.com>
2023-02-16 00:43:09 +0900

    style: :lipstick: 로그인 페이지 (내부) 레이아웃

--- frontend/src/pages/LoginPage/index.tsx
+++ frontend/src/pages/LoginPage/index.tsx
@@ -1,12 +1,16 @@
+import { FtLogoIcon } from 'assets';
 import { API } from 'common/constants';
+
+import { loginLinkStyle, loginPageLogoImageStyle, loginPageWrapperStyle } from './LoginPage.styles';
 
 export const LoginPage = () => {
   return (
-    <main>
-      <a href={process.env.REACT_APP_SERVER + API.FT_AUTH}>
-        <div>
-          <span>로그인</span>
-        </div>
+    <main className={loginPageWrapperStyle}>
+      <img src="logo.png" alt="pochitandence logo" width={280} height={80} className={loginPageLogoImageStyle} />
+      <a href={process.env.REACT_APP_SERVER + API.FT_AUTH} className={loginLinkStyle}>
+        <span>Login with</span>
+        <FtLogoIcon />
+        <span>OAuth</span>
       </a>
     </main>
   );
