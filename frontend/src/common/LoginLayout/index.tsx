1db50e0b9e4951f5446260331a47efce616f0e06
chichoon <chichoon.choi@gmail.com>
2023-02-16 00:59:48 +0900

    style: :lipstick: 로그인 레이아웃

--- frontend/src/common/LoginLayout/index.tsx
+++ frontend/src/common/LoginLayout/index.tsx
@@ -1,14 +1,27 @@
+import { GithubIcon } from 'assets';
 import { Outlet } from 'react-router-dom';
+import { githubLinkStyle, loginFooterStyle, loginHeaderStyle, loginMainWrapperStyle } from './LoginLayout.styles';
 
 export const LoginLayout = () => {
+  const isSamurai = Math.random() < 0.2;
+
   return (
     <>
-      <header>
-        <span>트센</span>
+      <header className={loginHeaderStyle}>
+        <img
+          src={isSamurai ? 'samurai.png' : 'csm.png'}
+          alt={isSamurai ? 'samurai-sword' : 'chainsaw-man'}
+          width={600}
+          height={350}
+        />
       </header>
-      <Outlet />
-      <footer>
-        <span>ft_transcendence</span>
+      <div className={loginMainWrapperStyle}>
+        <Outlet />
+      </div>
+      <footer className={loginFooterStyle}>
+        <a href="https://github.com/42Blank/ft_transcendence" className={githubLinkStyle}>
+          <GithubIcon />
+        </a>
       </footer>
     </>
   );
