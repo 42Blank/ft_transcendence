3159aa6256fa77af55319996df0a6362c4c718d3
Kang Jisoon <wltnskjs@naver.com>
2023-02-15 15:08:59 +0900

    feat: :sparkles: Pong 하단에 들어갈 게임 설명 예시 추가

--- frontend/src/pages/GamePage/index.tsx
+++ frontend/src/pages/GamePage/index.tsx
@@ -1,5 +1,12 @@
-import GameExample from './game';
+import GamePong from './game';
 
 export const GamePage = () => {
-  return <GameExample />;
+  return (
+    <div>
+      <GamePong />
+      {/* 게임 설명 예시 */}
+      <div>•BALL WILL SERVE AUTOMATICALLY</div>
+      <div>•AVOID MISSING BALL FOR HIGH SCORE</div>
+    </div>
+  );
 };
