8ceb3fd795e153c2bf13ad1d465374aced2135f2
huni <petercha2000@gmail.com>
2023-02-17 17:09:59 +0900

    fix: 입/출력 type 조금 바꿈

--- backend/src/socket/game/game.gateway.ts
+++ backend/src/socket/game/game.gateway.ts
@@ -1,6 +1,7 @@
 import { Logger, UseFilters } from '@nestjs/common';
 import {
   ConnectedSocket,
+  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
   SubscribeMessage,
@@ -27,16 +28,17 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @SubscribeMessage('ping')
   public async ping(
     @ConnectedSocket() client: SocketWithUser, //
+    @MessageBody() data: { message: string },
   ): Promise<void> {
     client.emit('pong', {
-      message: 'pong',
+      message: data.message,
     });
   }
 
   public async emitHello(client: SocketWithUser): Promise<void> {
     this.logger.verbose(`hello ${client.user.nickname}`);
 
-    this.io.to(client.id).emit('hello', client.user.nickname);
+    this.io.to(client.id).emit('hello', { nickname: client.user.nickname });
   }
 
   public async handleConnection(client: SocketWithUser): Promise<void> {
