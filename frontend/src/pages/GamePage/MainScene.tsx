6130e703a3c86e536877613322fc4c1532fc3aee
Kang Jisoon <wltnskjs@naver.com>
2023-02-17 16:15:57 +0900

    refactor: :recycle: Pong 변수명 직관적으로 변경 및 리팩토링

--- frontend/src/pages/GamePage/MainScene.tsx
+++ frontend/src/pages/GamePage/MainScene.tsx
@@ -1,16 +1,18 @@
 import Phaser from 'phaser';
 
+const scoreFontStyle = { fontSize: '32px', fontFamily: 'Arial' };
 export class MainScene extends Phaser.Scene {
   private ball: Phaser.Physics.Arcade.Image;
-  private paddle1: Phaser.Physics.Arcade.Image;
-  private paddle2: Phaser.Physics.Arcade.Image;
+  private paddleLeft: Phaser.Physics.Arcade.Image;
+  private paddleRight: Phaser.Physics.Arcade.Image;
 
-  private score1: number;
-  private scoreLabel1: Phaser.GameObjects.Text;
-  private score2: number;
-  private scoreLabel2: Phaser.GameObjects.Text;
+  private scoreLeft: number;
+  private scoreLabelLeft: Phaser.GameObjects.Text;
+  private scoreRight: number;
+  private scoreLabelRight: Phaser.GameObjects.Text;
 
   private key: Phaser.Types.Input.Keyboard.CursorKeys;
+
   /**
    * CursorKeys: 지정된 일부 키보드 값만 받을 수 있음
    * - Up, Down, Right, Left, Shift, SpaceBar
@@ -30,52 +32,56 @@ export class MainScene extends Phaser.Scene {
     this.ball.setBounce(1);
     this.ball.setVelocity(200, 200);
 
-    this.paddle1 = this.physics.add.image(100, 300, 'peddal');
-    this.paddle1.setImmovable(true);
-    this.paddle2 = this.physics.add.image(700, 300, 'peddal');
-    this.paddle2.setImmovable(true);
+    this.paddleLeft = this.physics.add.image(100, 300, 'peddal');
+    this.paddleLeft.setImmovable(true);
+    this.paddleRight = this.physics.add.image(700, 300, 'peddal');
+    this.paddleRight.setImmovable(true);
 
-    this.score1 = 0;
-    this.scoreLabel1 = this.add.text(200, 125, '0', { fontSize: '32px', fontFamily: 'Arial' }).setOrigin(0.5, 0.5);
-    this.score2 = 0;
-    this.scoreLabel2 = this.add.text(600, 125, '0', { fontSize: '32px', fontFamily: 'Arial' }).setOrigin(0.5, 0.5);
-    this.physics.add.collider(this.ball, this.paddle1, null, null, this);
-    this.physics.add.collider(this.ball, this.paddle2, null, null, this);
+    this.scoreLeft = 0;
+    this.scoreLabelLeft = this.add.text(200, 125, '0', scoreFontStyle).setOrigin(0.5, 0.5);
+    this.scoreRight = 0;
+    this.scoreLabelRight = this.add.text(600, 125, '0', scoreFontStyle).setOrigin(0.5, 0.5);
+
+    /**
+     * @param collideCallback 으로 패들 출돌 위치에 따라 velocity를 다르게 주면 되지 않을까?
+     */
+    this.physics.add.collider(this.ball, this.paddleLeft, null, null, this);
+    this.physics.add.collider(this.ball, this.paddleRight, null, null, this);
 
     this.key = this.input.keyboard.createCursorKeys();
   }
 
+  initBall() {
+    this.ball.setVisible(false);
+    this.ball.setPosition(400, 300);
+    this.ball.setVelocity(0, 0);
+    this.time.delayedCall(1500, () => {
+      this.ball.setVelocity(200, 200);
+    });
+    this.ball.setVisible(true);
+  }
+
   update(time: number, delta: number) {
-    if (this.paddle1 && this.paddle2 && this.key) {
-      if (this.key.up.isDown) this.paddle2.y -= 10;
-      else if (this.key.down.isDown) this.paddle2.y += 10;
-      if (this.key.shift.isDown) this.paddle1.y -= 10;
-      else if (this.key.space.isDown) this.paddle1.y += 10;
+    if (this.paddleLeft && this.paddleRight && this.key) {
+      if (this.key.up.isDown) this.paddleRight.y -= 10;
+      else if (this.key.down.isDown) this.paddleRight.y += 10;
+      if (this.key.shift.isDown) this.paddleLeft.y -= 10;
+      else if (this.key.space.isDown) this.paddleLeft.y += 10;
     }
     /* Paddle 임시 충돌 판정 코드 */
-    this.paddle1.y = Phaser.Math.Clamp(this.paddle1.y, 50, 550);
-    this.paddle2.y = Phaser.Math.Clamp(this.paddle2.y, 50, 550);
+    this.paddleLeft.y = Phaser.Math.Clamp(this.paddleLeft.y, 50, 550);
+    this.paddleRight.y = Phaser.Math.Clamp(this.paddleRight.y, 50, 550);
 
     if (this.ball.x < 10) {
-      this.score2 += 1;
-      this.scoreLabel2.text = this.score2.toString();
-      this.ball.setVisible(false);
-      this.ball.setPosition(400, 300);
-      this.ball.setVelocity(0, 0);
-      this.time.delayedCall(1500, () => {
-        this.ball.setVelocity(200, 200);
-        this.ball.setVisible(true);
-      });
+      this.scoreRight += 1;
+      this.scoreLabelRight.text = this.scoreRight.toString();
+
+      this.initBall();
     } else if (this.ball.x > 790) {
-      this.score1 += 1;
-      this.scoreLabel1.text = this.score1.toString();
-      this.ball.setVisible(false);
-      this.ball.setPosition(400, 300);
-      this.ball.setVelocity(0, 0);
-      this.time.delayedCall(1500, () => {
-        this.ball.setVelocity(-200, -200);
-        this.ball.setVisible(true);
-      });
+      this.scoreLeft += 1;
+      this.scoreLabelLeft.text = this.scoreLeft.toString();
+
+      this.initBall();
     }
   }
 }
