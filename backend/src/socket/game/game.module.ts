07531e22eb7b6c8fca87e6ba0e8b3caacb63513e
huni <petercha2000@gmail.com>
2023-02-18 20:22:08 +0900

    feat: gateway 추가

--- backend/src/socket/game/game.module.ts
+++ backend/src/socket/game/game.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../../common/database/entities/user.entity';
 import { ConnectionHandleModule } from '../connection-handle';
 import { GameGateway } from './game.gateway';
+import { GameRoomRepository } from './repository/game-room.repository';
+import { GameRoomService } from './service/game-room.service';
 
 @Module({
-  imports: [ConnectionHandleModule],
-  providers: [GameGateway],
+  imports: [TypeOrmModule.forFeature([User]), ConnectionHandleModule],
+  providers: [GameRoomRepository, GameRoomService, GameGateway],
 })
 export class GameModule {}
