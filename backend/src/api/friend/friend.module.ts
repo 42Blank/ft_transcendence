0fac7a6c9183c498076960412282458b1a4ca54d
jasong <jasong@student.42seoul.kr>
2023-02-18 17:42:34 +0900

    feat: find user service 추가

--- backend/src/api/friend/friend.module.ts
+++ backend/src/api/friend/friend.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FindUserService } from 'api/user/service/find-user.service';
 import { Friend } from '../../common/database/entities/friend.entity';
 import { User } from '../../common/database/entities/user.entity';
 import { FriendController } from './friend.controller';
+import { AddFriendService } from './service/add-friend.service';
 import { FindFriendService } from './service/find-friend.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Friend])],
-  providers: [FindFriendService],
+  providers: [FindFriendService, AddFriendService, FindUserService],
   controllers: [FriendController],
 })
 
