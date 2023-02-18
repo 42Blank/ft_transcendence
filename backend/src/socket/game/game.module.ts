b8b51ef9e89e64070bace6513331390d1bd64d1f
huni <petercha2000@gmail.com>
2023-02-19 01:54:27 +0900

    fix: 디펜던시 빠진거수정

--- backend/src/socket/game/game.module.ts
+++ backend/src/socket/game/game.module.ts
@@ -5,9 +5,10 @@ import { ConnectionHandleModule } from '../connection-handle';
 import { GameGateway } from './game.gateway';
 import { GameRoomRepository } from './repository/game-room.repository';
 import { GameRoomService } from './service/game-room.service';
+import { GameUserService } from './service/game-user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), ConnectionHandleModule],
-  providers: [GameRoomRepository, GameRoomService, GameGateway],
+  providers: [GameRoomRepository, GameRoomService, GameUserService, GameGateway],
 })
 export class GameModule {}
