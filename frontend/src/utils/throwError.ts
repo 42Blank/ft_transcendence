9a7b91259cc193a83051020d20c34ee5b4ee1daa
chichoon <chichoon.choi@gmail.com>
2023-02-18 21:21:29 +0900

    chore: :fire: 필요없는 예외처리용 코드 삭제

--- frontend/src/utils/throwError.ts
+++
@@ -1,6 +0,0 @@
-import { AxiosError } from 'axios';
-
-export function throwError(error: Error | AxiosError) {
-  if (error instanceof AxiosError) throw Error(error.response?.data ?? 'Unknown error');
-  throw error;
-}
