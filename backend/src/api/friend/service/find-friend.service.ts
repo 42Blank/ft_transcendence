1e8db76d170cfb57ce9fef157f77277b71a2c94d
jasong <jasong@student.42seoul.kr>
2023-02-18 18:09:43 +0900

    fix: senderId where 구분 오타 수정

--- backend/src/api/friend/service/find-friend.service.ts
+++ backend/src/api/friend/service/find-friend.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Friend } from '../../../common/database/entities/friend.entity';
 import { User } from '../../../common/database/entities/user.entity';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class FindFriendService {
@@ -13,18 +14,22 @@ export class FindFriendService {
     private readonly friendRepository: Repository<Friend>,
   ) {}
 
-  async findAllFriendsByUserId(sendFriendRequestUserId: number): Promise<User[]> {
+  async findAllFriendsByUserId(senderId: number): Promise<User[]> {
     const friends = await this.friendRepository.find({
-      where: { sendFriendRequestUserId },
+      where: { sendFriendRequestUserId: senderId },
     });
     const friendIds = friends.map(friend => friend.recvFriendRequestUserId && friend.status === 'FRIEND' ? friend.recvFriendRequestUserId : null);
 
     return this.userRepository.findByIds(friendIds);
   }
 
-  async findFriendByIdOrFail(friendId: number): Promise<Friend> {
-    return this.friendRepository.findOneOrFail({
-      where: { id: friendId },
+  async findFriendByIdOrFail(senderId: number, friendId: number): Promise<Friend> {
+    const friend = await this.friendRepository.findOne({
+      where: { sendFriendRequestUserId: senderId, recvFriendRequestUserId: friendId },
     });
+    if (!friend) {
+      throw new NotFoundException(`Friend with id ${friendId} not found`);
+    }
+    return friend;
   }
 }
