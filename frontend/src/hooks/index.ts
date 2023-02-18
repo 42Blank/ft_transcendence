b7df815db92c7b75f94202d179f04f97a59a9d35
chichoon <chichoon.choi@gmail.com>
2023-02-18 21:30:52 +0900

    feat: :sparkles: useLogin 추가
    조건부 useQuery 잘 돌아가는지 체크해봐야함 근데 저 로그인 안됐다고 뜸 ㅋㅋ

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,4 +1,5 @@
 export { useHandleSocket } from './useHandleSocket';
 export { useGetUser } from './useGetUser';
 export { useGetCurrentChatRoom } from './useGetCurrentChatRoom';
+export { useLogin } from './useLogin';
 export { useLogout } from './useLogout';
---
+++ frontend/src/hooks/useLogin.ts
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+import { ROUTE } from 'common/constants';
+import { getFtCallbackCode, getLogin } from 'services';
+import { FtProfileType } from 'types/user';
+
+export function useLogin() {
+  const [param] = useSearchParams();
+  const nav = useNavigate();
+  const [isRegistered, setIsRegistered] = useState<boolean>(false);
+  useQuery(['ft-auth'], () => getFtCallbackCode(param.get('code')), {
+    refetchOnWindowFocus: false,
+    staleTime: 0,
+    cacheTime: 0,
+    retry: 0,
+    useErrorBoundary: false,
+    onSuccess: (data: FtProfileType) => {
+      console.log(data);
+      if (!data.isRegistered) nav(ROUTE.REGISTER);
+      setIsRegistered(true);
+    },
+    onError: () => {
+      // 로그인 실패
+    },
+  });
+
+  useQuery(['user-me'], getLogin, {
+    refetchOnWindowFocus: false,
+    staleTime: 1000 * 60 * 60 * 12,
+    cacheTime: 1000 * 60 * 60 * 12, // TODO: 적절한 시간으로 수정
+    retry: 0,
+    enabled: isRegistered,
+    useErrorBoundary: false,
+    onSuccess: () => {
+      console.log('댄저러스');
+      nav(ROUTE.CHAT); // 일단해봄
+    },
+    onError: () => {
+      // 로그인 실패
+    },
+  });
+}
