6ef55ac7222c3dacb56dbfdadc98353443b01840
chichoon <chichoon.choi@gmail.com>
2023-02-19 01:02:20 +0900

    chore: :truck: href => uri

--- frontend/src/services/axiosWrapper.ts
+++ frontend/src/services/axiosWrapper.ts
@@ -1,24 +1,24 @@
 import axios from 'axios';
 import { throwApiError } from 'utils/error';
 
-export async function axiosGet<Type>(href: string): Promise<Type> {
+export async function axiosGet<Type>(uri: string): Promise<Type> {
   return axios
-    .get<Type>(`${process.env.REACT_APP_SERVER}${href}`)
+    .get<Type>(`${process.env.REACT_APP_SERVER}${uri}`)
     .then(({ data }) => data)
     .catch(throwApiError);
 }
 
-export async function axiosPost<bodyObjType, resType>(href: string, reqData: bodyObjType): Promise<resType> {
+export async function axiosPost<bodyObjType, resType>(uri: string, reqData: bodyObjType): Promise<resType> {
   return axios
-    .post<resType>(`${process.env.REACT_APP_SERVER}${href}`, reqData)
+    .post<resType>(`${process.env.REACT_APP_SERVER}${uri}`, reqData)
     .then(({ data }) => data)
     .catch(throwApiError);
 }
 
-export async function axiosPut<bodyObjType>(href: string, reqData: bodyObjType): Promise<void> {
-  return axios.put(`${process.env.REACT_APP_SERVER}${href}`, reqData);
+export async function axiosPut<bodyObjType>(uri: string, reqData: bodyObjType): Promise<void> {
+  return axios.put(`${process.env.REACT_APP_SERVER}${uri}`, reqData);
 }
 
-export async function axiosDelete(href: string): Promise<void> {
-  return axios.delete(`${process.env.REACT_APP_SERVER}${href}`);
+export async function axiosDelete(uri: string): Promise<void> {
+  return axios.delete(`${process.env.REACT_APP_SERVER}${uri}`);
 }
