06ad2a65c54288ae4685441b3c77e6863d756367
jasong <jasong@student.42seoul.kr>
2023-02-19 02:28:21 +0900

    fix: FindService 대신 UserRepository에서 Find하는 것으로 변경

--- backend/src/api/friend/service/add-friend.service.ts
+++ backend/src/api/friend/service/add-friend.service.ts
@@ -2,33 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Friend } from '../../../common/database/entities/friend.entity';
-import { recvFriendRequestDto } from '../dto/request/recv-friend.dto';
-import { FindFriendService } from './find-friend.service';
+import { FriendRequestDto } from '../dto/request/friend-request.dto';
 import { BadRequestException } from '@nestjs/common';
-import { FindUserService } from 'api/user/service/find-user.service';
+import { User } from '../../../common/database/entities/user.entity';
 
 @Injectable()
 export class AddFriendService {
   constructor(
     @InjectRepository(Friend)
     private readonly friendRepository: Repository<Friend>,
-    private readonly findFriendService: FindFriendService,
-    private readonly findUserService: FindUserService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
 
-  async addFriendByFriendId(userid: number, recvFriendRequestDto: recvFriendRequestDto): Promise<recvFriendRequestDto> {
+  async addFriendByReceiverId(userid: number, recvFriendRequestDto: FriendRequestDto): Promise<void> {
     if (userid === recvFriendRequestDto.recvFriendRequestUserId) {
       throw new BadRequestException (`자기 자신을 친구로 추가할 수 없습니다.`);
     }
-    await this.findUserService.findOneByIdOrFail(recvFriendRequestDto.recvFriendRequestUserId);
-    const isFriend = await this.findFriendService.findFriendById(userid, recvFriendRequestDto.recvFriendRequestUserId);
-    if (!!isFriend) {
-      throw new BadRequestException (`이미 친구입니다.`);
+    if (await this.userRepository.findOne({ where: { id: recvFriendRequestDto.recvFriendRequestUserId } }) === undefined) {
+      throw new BadRequestException (`존재하지 않는 사용자입니다.`);
+    }
+    if (await this.friendRepository.findOne({ where: { sendFriendRequestUserId: userid, recvFriendRequestUserId: recvFriendRequestDto.recvFriendRequestUserId } }) !== undefined) {
+      throw new BadRequestException (`이미 친구 요청을 보냈습니다.`);
     }
     await this.friendRepository.save({
       sendFriendRequestUserId: userid,
       ...recvFriendRequestDto,
     });
-    return recvFriendRequestDto;
   }
 }
