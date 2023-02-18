57235b6e501e4b36e532fb34d1292e150e0936e2
jasong <jasong@student.42seoul.kr>
2023-02-18 19:59:41 +0900

    fix: add friend 친구 아닌 상태면 bad request throw

--- backend/src/api/friend/service/add-friend.service.ts
+++ backend/src/api/friend/service/add-friend.service.ts
@@ -2,19 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Friend } from '../../../common/database/entities/friend.entity';
-import { CreateFriendRequestDto } from '../dto/request/create-friend.dto';
+import { recvFriendRequestDto } from '../dto/request/recv-friend.dto';
+import { FindFriendService } from './find-friend.service';
+import { BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class AddFriendService {
   constructor(
     @InjectRepository(Friend)
     private readonly friendRepository: Repository<Friend>,
+    private readonly findFriendService: FindFriendService,
   ) {}
 
-  async addFriendByFriendId(userid: number, createFriendRequestDto: CreateFriendRequestDto): Promise<void> {
+  async addFriendByFriendId(userid: number, recvFriendRequestDto: recvFriendRequestDto): Promise<recvFriendRequestDto> {
+    const friend = await this.findFriendService.findFriendByIdOrFail(userid, recvFriendRequestDto.recvFriendRequestUserId);
+    if (friend) {
+      throw new BadRequestException (`이미 친구입니다.`);
+    }
     await this.friendRepository.save({
       sendFriendRequestUserId: userid,
-      ...createFriendRequestDto,
+      ...recvFriendRequestDto,
     });
+    return recvFriendRequestDto;
   }
 }
