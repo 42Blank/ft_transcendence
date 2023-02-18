17c11f0f273962140c1d8cedcc8bdb5b7c2bdd73
jasong <jasong@student.42seoul.kr>
2023-02-18 17:43:00 +0900

    chore: friend dto 네이밍 변경

--- backend/src/api/friend/dto/request/create-friend.dto.ts
+++ backend/src/api/friend/dto/request/create-friend.dto.ts
@@ -1,17 +1,13 @@
+// Is this useless?
 import { IsNotEmpty, IsInt, IsEnum } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { FriendStatus } from '../../../../common/database/entities/friend.entity';
 
-export class UpdateFriendRequestDto {
+export class CreateFriendRequestDto {
   @IsInt()
   @IsNotEmpty()
   @ApiProperty({example: '1', description: '친구 요청 받은 유저 고유번호'})
   recvFriendRequestUserId: number;
-
-  @IsInt()
-  @IsNotEmpty()
-  @ApiProperty({example: '2', description: '친구 요청한 유저 고유번호'})
-  sendFriendRequestUserId: number;
 
   @IsEnum(['FRIEND', 'BLOCK'])
   @IsNotEmpty()
