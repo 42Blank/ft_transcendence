dbe4fd26435e2a90804152cf0b02fcb1a5ea1901
jasong <jasong@student.42seoul.kr>
2023-02-18 20:34:34 +0900

    fix: 친구추가 있는 user만 가능함

--- backend/src/api/friend/service/add-friend.service.ts
+++ backend/src/api/friend/service/add-friend.service.ts
@@ -5,6 +5,7 @@ import { Friend } from '../../../common/database/entities/friend.entity';
 import { recvFriendRequestDto } from '../dto/request/recv-friend.dto';
 import { FindFriendService } from './find-friend.service';
 import { BadRequestException } from '@nestjs/common';
+import { FindUserService } from 'api/user/service/find-user.service';
 
 @Injectable()
 export class AddFriendService {
@@ -12,11 +13,16 @@ export class AddFriendService {
     @InjectRepository(Friend)
     private readonly friendRepository: Repository<Friend>,
     private readonly findFriendService: FindFriendService,
+    private readonly findUserService: FindUserService,
   ) {}
 
   async addFriendByFriendId(userid: number, recvFriendRequestDto: recvFriendRequestDto): Promise<recvFriendRequestDto> {
-    const friend = await this.findFriendService.findFriendByIdOrFail(userid, recvFriendRequestDto.recvFriendRequestUserId);
-    if (friend) {
+    if (userid === recvFriendRequestDto.recvFriendRequestUserId) {
+      throw new BadRequestException (`자기 자신을 친구로 추가할 수 없습니다.`);
+    }
+    await this.findUserService.findOneByIdOrFail(recvFriendRequestDto.recvFriendRequestUserId);
+    const isFriend = await this.findFriendService.findFriendById(userid, recvFriendRequestDto.recvFriendRequestUserId);
+    if (!!isFriend) {
       throw new BadRequestException (`이미 친구입니다.`);
     }
     await this.friendRepository.save({
