bf572eea5d8c6abb56d12218313112c7c0970844
jasong <jasong@student.42seoul.kr>
2023-02-19 02:28:38 +0900

    fix: remove 대신 delete에 조건 넣어서 삭제로 변경

--- backend/src/api/friend/service/delete-friend.service.ts
+++ backend/src/api/friend/service/delete-friend.service.ts
@@ -2,20 +2,19 @@ import { BadRequestException, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Friend } from "../../../common/database/entities/friend.entity";
-import { recvFriendRequestDto } from "../dto/request/recv-friend.dto";
-import { FindFriendService } from "./find-friend.service";
+import { DeleteFriendRequestDto } from "../dto/request/delete-friend-request.dto";
 
 @Injectable()
 export class DeleteFriendService {
   constructor(
     @InjectRepository(Friend)
     private readonly friendRepository: Repository<Friend>,
-    private readonly findFriendService: FindFriendService,
   ) {}
 
-  async deleteFriendByFriendId(userid: number, recvDto: recvFriendRequestDto): Promise<recvFriendRequestDto> {
-    const friend = await this.findFriendService.findFriendByIdOrFail(userid, recvDto.recvFriendRequestUserId);
-    await this.friendRepository.remove(friend);   
-    return recvDto;
+  async deletebyId(userid: number, deleteDto: DeleteFriendRequestDto): Promise<void> {
+    await this.friendRepository.delete({
+      sendFriendRequestUserId: userid,
+      recvFriendRequestUserId: deleteDto.recvFriendRequestUserId,
+    });
   }
 }
