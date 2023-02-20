5a6af606e3355f08bcd39bf0ff2542c7ef506f68
stripy1026 <inwoo1026@naver.com>
2023-02-20 16:51:40 +0900

    feat: :sparkles: refresh page when click button

--- frontend/src/pages/ProfilePage/ManageFriends.tsx
+++ frontend/src/pages/ProfilePage/ManageFriends.tsx
@@ -6,16 +6,14 @@ import { UserInfoType } from 'types/user';
 type UserState = 'Friend' | 'No' | 'Block';
 
 export const ManageFriends = ({ user }: { user: UserInfoType }) => {
-  const { friendList, blockList } = useGetUserList();
+  const { friendList, blockList, refetchFriendList, refetchBlockList } = useGetUserList();
 
   function checkFriend(userList: UserInfoType): Boolean {
     return user.id === userList.id;
   }
 
   function classifyFriend(): UserState {
-    if (friendList.some(checkFriend)) {
-      return 'Friend';
-    }
+    if (friendList.some(checkFriend)) return 'Friend';
     if (blockList.some(checkFriend)) return 'Block';
     return 'No';
   }
@@ -23,11 +21,17 @@ export const ManageFriends = ({ user }: { user: UserInfoType }) => {
   const friendState: UserState = classifyFriend();
 
   function handleClickUnfriendOrBlock() {
-    deleteFriend(user.id);
+    deleteFriend(user.id).then(() => {
+      refetchFriendList();
+      refetchBlockList();
+    });
   }
 
   function handleClickFriendOrBlock(userID: number, status: 'FRIEND' | 'BLOCK') {
-    postFriendOrBlock({ recvFriendRequestUserId: userID, status });
+    postFriendOrBlock({ recvFriendRequestUserId: userID, status }).then(() => {
+      refetchFriendList();
+      refetchBlockList();
+    });
   }
 
   return (
