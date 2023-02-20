8bf25faeeb75aac6056fb8007abb0d11a208c15e
chichoon <chichoon.choi@gmail.com>
2023-02-20 23:21:01 +0900

    chore: :fire: 결국 안 쓰는 파일 삭제, axios.default 문 삭제 (instance 로 관리)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -16,8 +15,6 @@ const queryClient = new QueryClient({
     },
   },
 });
-
-axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
