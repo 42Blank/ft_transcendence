e12cab53e9564331a448a4549bae18fcede2cb46
stripy1026 <inwoo1026@naver.com>
2023-02-20 19:45:28 +0900

    feat: :construction: Add dummy achievement

--- frontend/src/pages/ProfilePage/Achievement.tsx
+++ frontend/src/pages/ProfilePage/Achievement.tsx
@@ -2,6 +2,15 @@ interface Props {
   userId: number;
 }
 
+const DUMMY_ACHIEVEMENT = [
+  {
+    id: 1,
+    name: 'First Blood',
+    description: 'Winning very first game',
+    image: '/pochita_sample.png',
+  },
+];
+
 export const Achievement = ({ userId }: Props) => {
   return <div>Achievement ID: {`${userId}`}</div>;
 };
