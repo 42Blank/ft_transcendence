6fe09b8775c7e1a6eb8862afa2d4afb5463d4ec6
stripy1026 <inwoo1026@naver.com>
2023-02-20 20:04:42 +0900

    feat: :sparkles: Display Achievement using dummy

--- frontend/src/pages/ProfilePage/Achievement.tsx
+++ frontend/src/pages/ProfilePage/Achievement.tsx
@@ -1,5 +1,14 @@
+import { tmpAvatarStyle } from './tmpAvatarStyle';
+
 interface Props {
   userId: number;
+}
+
+interface AchievementType {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
 }
 
 const DUMMY_ACHIEVEMENT = [
@@ -9,8 +18,25 @@ const DUMMY_ACHIEVEMENT = [
     description: 'Winning very first game',
     image: '/pochita_sample.png',
   },
+  {
+    id: 3,
+    name: 'Killing Spree',
+    description: 'Winning 3 games in a streak',
+    image: '/pochita_sample.png',
+  },
 ];
 
 export const Achievement = ({ userId }: Props) => {
-  return <div>Achievement ID: {`${userId}`}</div>;
+  return (
+    <>
+      <h1>Achievement List: {`${userId}`}</h1>
+      {DUMMY_ACHIEVEMENT.map((value: AchievementType) => (
+        <div key={value.id}>
+          <img className={tmpAvatarStyle} src={value.image} alt="pochi" />
+          <span>name: {value.name}</span>
+          <span>description: {value.description}</span>
+        </div>
+      ))}
+    </>
+  );
 };
