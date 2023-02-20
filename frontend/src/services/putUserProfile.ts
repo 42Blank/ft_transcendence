3676251c3f34f9de14f0dd29645f309e59a62805
chichoon <chichoon.choi@gmail.com>
2023-02-20 23:18:48 +0900

    fix: :bug: nickname, avatar 중 하나만 들어와도 되도록 설정 (선택적 매개변수)

--- frontend/src/services/putUserProfile.ts
+++ frontend/src/services/putUserProfile.ts
@@ -2,8 +2,8 @@ import { API } from 'common/constants';
 import { axiosPut } from './axiosWrapper';
 
 interface Params {
-  nickname: string;
-  avatar: string;
+  nickname?: string;
+  avatar?: string;
 }
 
 export function putUserProfile({ nickname, avatar }: Params): Promise<void> {
