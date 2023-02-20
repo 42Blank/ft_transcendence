7a76263bc7d16ed294403947da0ffaaec4680dd4
stripy1026 <inwoo1026@naver.com>
2023-02-20 20:56:31 +0900

    feat: :sparkles: Add chievementAt,  onFocus Event

--- frontend/src/pages/ProfilePage/Achievement.tsx
+++ frontend/src/pages/ProfilePage/Achievement.tsx
@@ -17,26 +17,38 @@ const DUMMY_ACHIEVEMENT = [
     name: 'First Blood',
     description: 'Winning very first game',
     image: '/pochita_sample.png',
+    achievedAt: '1970-01-01T00:00:00.000Z',
   },
   {
     id: 3,
     name: 'Killing Spree',
     description: 'Winning 3 games in a streak',
     image: '/pochita_sample.png',
+    achievedAt: '1970-01-01T00:00:00.000Z',
   },
 ];
 
 export const Achievement = ({ userId }: Props) => {
+  function handleTest(value: AchievementType) {
+    return <div>{value.description}</div>;
+  }
+
   return (
-    <>
+    <main>
       <h1>Achievement List: {`${userId}`}</h1>
       {DUMMY_ACHIEVEMENT.map((value: AchievementType) => (
         <div key={value.id}>
-          <img className={tmpAvatarStyle} src={value.image} alt="pochi" />
+          <img
+            className={tmpAvatarStyle}
+            src={value.image}
+            alt="pochi"
+            onMouseOver={() => handleTest(value)}
+            onFocus={() => handleTest(value)}
+          />
           <span>name: {value.name}</span>
           <span>description: {value.description}</span>
         </div>
       ))}
-    </>
+    </main>
   );
 };
