958525cef0d2cd8b2de98e08ff6446bcecbf40f4
Kang Jisoon <wltnskjs@naver.com>
2023-02-21 23:04:42 +0900

    feat: :sparkles: 게임 메뉴로 돌아가기 버튼 및 샘플 메세지 추가

--- frontend/src/pages/GameResultPage/index.tsx
+++ frontend/src/pages/GameResultPage/index.tsx
@@ -1,7 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
+import { ROUTE } from 'common/constants';
+
 export const GameResultPage = () => {
+  const navigate = useNavigate();
+
+  function handleClickLinkButton() {
+    navigate(ROUTE.GAME);
+    window.location.reload();
+  }
   return (
     <div>
       <h1>Result Page</h1>
+      <h4>Player VS Player</h4>
+      <h4>5 VS 3</h4>
+      <button type="button" onClick={handleClickLinkButton}>
+        <span>게임 메뉴로 돌아가기</span>
+      </button>
     </div>
   );
 };
