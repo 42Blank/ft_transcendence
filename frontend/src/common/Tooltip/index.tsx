04785e1a18d7e439eae17cf7f223e8317cc68cdc
stripy1026 <inwoo1026@naver.com>
2023-02-21 18:53:49 +0900

    feat: :bug: ESLint Error fixed
    Why defaultProp needs ?

--- frontend/src/common/Tooltip/index.tsx
+++ frontend/src/common/Tooltip/index.tsx
@@ -10,3 +10,8 @@ interface Props {
 export const Tooltip = ({ children, className }: Props) => {
   return <div className={`${tooltipWrapper} ${className}`}>{children}</div>;
 }; // position: relative 인 부모와 함께 사용
+
+Tooltip.defaultProps = {
+  className: null,
+};
+// Preventing error message from ESlint : propType "className" is not required, but has no corresponding defaultProps declaration.
