d3639b9f2922f2bd18c073e976599e320c747e13
stripy1026 <inwoo1026@naver.com>
2023-02-22 16:43:48 +0900

    Revert "test: :test_tube: setLeaveGameRoom 할 때 destroy 하려고 했으나 실패!"
    This reverts commit 2d7b6658d4d7959f727bc73917206ef46d171b1a.

--- frontend/src/pages/GamePage/index.tsx
+++ frontend/src/pages/GamePage/index.tsx
@@ -1,50 +1,23 @@
-import { useEffect, useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
-import { useGetCurrentGameRoom, useGetUser } from 'hooks';
+import { useGetCurrentGameRoom } from 'hooks';
 import { useSetRecoilState } from 'recoil';
 import { leaveGameRoomState } from 'store';
-
-import Phaser from 'phaser';
-import { GameInstance, IonPhaser } from '@ion-phaser/react';
-
-import { MainScene } from './MainScene';
-
-const mainScene = new MainScene();
-
-const game: GameInstance = {
-  width: 800,
-  height: 600,
-  type: Phaser.AUTO,
-  scene: mainScene,
-  physics: { default: 'arcade', arcade: { gravity: { y: 0 } } },
-};
+import GamePong from './game';
 
 export const GamePage = () => {
   const currentGameRoom = useGetCurrentGameRoom();
   const setLeaveGameRoom = useSetRecoilState(leaveGameRoomState);
-
-  const gameRef = useRef<HTMLIonPhaserElement>(null);
-  const [initialize] = useState(true);
-
-  const { data: currentUser } = useGetUser();
-
-  const isHost = currentUser.id === currentGameRoom.host.user.id;
-
-  mainScene.initHandlers();
-  mainScene.hostCheckHandlers(isHost);
-  mainScene.naviHandlers(useNavigate());
-
   useEffect(() => {
     return () => {
-      gameRef.current?.destroy();
       setLeaveGameRoom({ id: currentGameRoom.id });
     };
   }, []);
 
   return (
     <div>
-      <IonPhaser ref={gameRef} game={game} initialize={initialize} />
+      <GamePong />
+      {/* 게임 설명 예시 */}
       <div>• BALL WILL SERVE AUTOMATICALLY</div>
       <div>• AVOID MISSING BALL FOR HIGH SCORE</div>
     </div>
