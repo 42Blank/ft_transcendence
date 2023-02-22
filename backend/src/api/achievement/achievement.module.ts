c70bfafc9b2620979c9fc31c421db76c82fdd8f2
jasong <jasong@student.42seoul.kr>
2023-02-22 19:44:31 +0900

    feat: 업적 업데이트 module에 의존 파일들 추가

--- backend/src/api/achievement/achievement.module.ts
+++ backend/src/api/achievement/achievement.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Achievement } from 'common/database/entities/achievement.entity';
+import { Friend } from 'common/database/entities/friend.entity';
+import { MatchHistory } from 'common/database/entities/match-history.entity';
 import { UserAchievement } from 'common/database/entities/user-achievement.entity';
 import { User } from '../../common/database/entities/user.entity';
 import { AchievementController } from './achievement.controller';
 import { GetAchievementService } from './services/get-achievement.service';
+import { UpdateUserAchievementService } from './services/update-user-achievement.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Achievement, UserAchievement])],
-  providers: [GetAchievementService],
+  imports: [TypeOrmModule.forFeature([User, Achievement, UserAchievement, Friend, MatchHistory])],
+  providers: [GetAchievementService, UpdateUserAchievementService],
   controllers: [AchievementController],
 })
 export class AchievementModule {}
