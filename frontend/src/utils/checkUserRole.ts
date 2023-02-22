a0f217054f48d7a481e6b9f24c14eddc07b5c3af
chichoon <chichoon.choi@gmail.com>
2023-02-22 19:08:58 +0900

    fix: :bug: cannot read properties of undefined 문제 임시 해결
    채팅방에 유저가 없을 때 일어나는 버그이므로 에러 문구를 throw 하는 것으로 임시대처
    
    나중에바귈수이씅ㅁ

--- frontend/src/utils/checkUserRole.ts
+++ frontend/src/utils/checkUserRole.ts
@@ -1,5 +1,7 @@
 import { ChatUserInfoType, ChatUserRoleType } from 'types/chat';
 
 export function checkUserRole(users: ChatUserInfoType[], currentUserId: number): ChatUserRoleType {
-  return users.find(({ user }) => user.id === currentUserId).role;
+  const foundUser = users.find(({ user }) => user.id === currentUserId);
+  if (!foundUser) throw new Error('현재 채팅방에 있지 않습니다!');
+  return foundUser.role;
 }
