f24e5b77f2424cdb9e8fbb559c78503ef9ce5bf3
jasong <jasong@student.42seoul.kr>
2023-02-22 19:45:00 +0900

    fix: get achievement에서 userid 유효성 검사 및 함수 네이밍 수정

--- backend/src/api/achievement/services/get-achievement.service.ts
+++ backend/src/api/achievement/services/get-achievement.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Achievement } from 'common/database/entities/achievement.entity';
 import { UserAchievement } from 'common/database/entities/user-achievement.entity';
+import { User } from 'common/database/entities/user.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -9,9 +10,20 @@ export class GetAchievementService {
   constructor(
     @InjectRepository(UserAchievement)
     private readonly userAchievementRepository: Repository<UserAchievement>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
 
-  async getAchievement(userId: number): Promise<Achievement[]> {
+  async getAchievementById(userId: number): Promise<Achievement[]> {
+    // 여기서도 user not found를 체크해주는게 맞는지??
+    const user = this.userRepository.findOne({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) throw new NotFoundException(`User with id ${userId} not found`);
+
     const userAchievements = await this.userAchievementRepository.find({
       where: {
         userId: userId,
