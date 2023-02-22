fcc19e27b2d22d817627af406136bc719d54bf1e
jasong <jasong@student.42seoul.kr>
2023-02-22 18:33:17 +0900

    fix: user id 값으로 업적 받아오기로 변경

--- backend/src/api/achievement/achievement.controller.ts
+++ backend/src/api/achievement/achievement.controller.ts
@@ -1,20 +1,17 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { ReqUser, UserJwtAuthGuard } from 'common/auth/jwt-auth';
 import { Achievement } from 'common/database/entities/achievement.entity';
-import { User } from 'common/database/entities/user.entity';
 import { GetAchievementService } from './services/get-achievement.service';
 
 @ApiTags('Achievement')
 @Controller('achievement')
-@UseGuards(UserJwtAuthGuard)
 export class AchievementController {
   constructor(private readonly getAchievementService: GetAchievementService) {}
 
-  @Get()
-  @ApiOperation({ summary: '업적 목록 가져오기' })
-  @ApiOkResponse({ description: '내 업적 목록', type: Achievement, isArray: true })
-  async all(@ReqUser() { id }: User): Promise<Achievement[]> {
+  @Get(':id')
+  @ApiOperation({ summary: '유저 업적 목록 가져오기' })
+  @ApiOkResponse({ description: '유저 업적 목록', type: Achievement, isArray: true })
+  async all(@Param('id', ParseIntPipe) id: number): Promise<Achievement[]> {
     return await this.getAchievementService.getAchievement(id);
   }
 }
