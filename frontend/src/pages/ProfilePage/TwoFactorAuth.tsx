749c9b8f4657c96db74935be3d552ab4781c172d
stripy1026 <inwoo1026@naver.com>
2023-02-22 19:39:59 +0900

    feat: :sparkles: initialize TwoFactorAuth

--- frontend/src/pages/ProfilePage/TwoFactorAuth.tsx
+++ frontend/src/pages/ProfilePage/TwoFactorAuth.tsx
@@ -1,3 +1,32 @@
+import { useState } from 'react';
+
+import { Modal } from 'common';
+
+import { EditProfileModal } from './EditProfileModal';
+
 export const TwoFactorAuth = () => {
-  return <div>Two-factor Authentication</div>;
+  const [isModalShown, setModalShown] = useState<Boolean>(false);
+
+  function handleOpenModal() {
+    setModalShown(true);
+  }
+
+  function handleCloseModal() {
+    setModalShown(false);
+  }
+
+  return (
+    <>
+      <div>
+        <button type="button" onClick={handleOpenModal}>
+          Two-Factor Authentication
+        </button>
+      </div>
+      {isModalShown && (
+        <Modal onClickClose={handleCloseModal}>
+          <EditProfileModal onClickClose={handleCloseModal} />
+        </Modal>
+      )}
+    </>
+  );
 };
