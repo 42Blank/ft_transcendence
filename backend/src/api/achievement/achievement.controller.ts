60d84b229a1e9ae6d677a0bb65bae7b38bacebae
jasong <jasong@student.42seoul.kr>
2023-02-22 19:44:04 +0900

    feat: achievement controller 업데이트 추가

--- backend/src/api/achievement/achievement.controller.ts
+++ backend/src/api/achievement/achievement.controller.ts
@@ -1,17 +1,28 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Achievement } from 'common/database/entities/achievement.entity';
 import { GetAchievementService } from './services/get-achievement.service';
+import { UpdateUserAchievementService } from './services/update-user-achievement.service';
 
 @ApiTags('Achievement')
 @Controller('achievement')
 export class AchievementController {
-  constructor(private readonly getAchievementService: GetAchievementService) {}
+  constructor(
+    private readonly getAchievementService: GetAchievementService,
+    private readonly updateUserAchievementService: UpdateUserAchievementService,
+  ) {}
 
   @Get(':id')
   @ApiOperation({ summary: '유저 업적 목록 가져오기' })
   @ApiOkResponse({ description: '유저 업적 목록', type: Achievement, isArray: true })
   async all(@Param('id', ParseIntPipe) id: number): Promise<Achievement[]> {
-    return await this.getAchievementService.getAchievement(id);
+    return await this.getAchievementService.getAchievementById(id);
+  }
+
+  @Post(':id')
+  @ApiOperation({ summary: '유저 업적 업데이트' })
+  @ApiOkResponse({ description: '업적 업데이트 성공' })
+  async update(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return await this.updateUserAchievementService.updateAchievement(id);
   }
 }
