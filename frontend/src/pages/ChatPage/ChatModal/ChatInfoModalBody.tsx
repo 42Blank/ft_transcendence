a53b48a714c169b09e64562bfb019376fc7d5ca5
chichoon <chichoon.choi@gmail.com>
2023-02-22 18:35:35 +0900

    feat: :sparkles: 차단 목록 출력 기능 추가
    모달에서 탭을 눌러 결정할 수 있다는

--- frontend/src/pages/ChatPage/ChatModal/ChatInfoModalBody.tsx
+++ frontend/src/pages/ChatPage/ChatModal/ChatInfoModalBody.tsx
@@ -1,19 +1,60 @@
+import { useState } from 'react';
+
 import { ChatUserInfoType, ChatUserRoleType } from 'types/chat';
+import { UserInfoType } from 'types/user';
 import { ChatUserListElement } from './ChatUserListElement';
 
-import { chatInfoModalBodyStyle } from './ChatInfoModalBody.styles';
+import {
+  chatInfoModalBodyButtonStyle,
+  chatInfoModalBodyButtonWrapperStyle,
+  chatInfoModalBodyStyle,
+} from './ChatInfoModalBody.styles';
+import { BannedUserListElement } from './BannedUserListElement';
 
 interface Props {
   users: ChatUserInfoType[];
+  bannedUsers: UserInfoType[];
   currentUserRole: ChatUserRoleType; // TODO: 헷갈릴 여지 있는 변수명?
 }
 
-export const ChatInfoModalBody = ({ users, currentUserRole }: Props) => {
+export const ChatInfoModalBody = ({ users, bannedUsers, currentUserRole }: Props) => {
+  const [isBanListShown, setIsBanListShown] = useState(false);
+
+  function handleClickUserListButton() {
+    setIsBanListShown(false);
+  }
+
+  function handleClickBanListButton() {
+    setIsBanListShown(true);
+  }
+
   return (
-    <ul className={chatInfoModalBodyStyle}>
-      {users.map((user, index) => (
-        <ChatUserListElement key={`chat-user-list-${index}`} chatUser={user} currentUserRole={currentUserRole} />
-      ))}
-    </ul>
+    <>
+      <div className={chatInfoModalBodyButtonWrapperStyle}>
+        <button
+          type="button"
+          className={chatInfoModalBodyButtonStyle(!isBanListShown)}
+          onClick={handleClickUserListButton}
+        >
+          <span>유저 목록</span>
+        </button>
+        <button
+          type="button"
+          className={chatInfoModalBodyButtonStyle(isBanListShown)}
+          onClick={handleClickBanListButton}
+        >
+          <span>차단 목록</span>
+        </button>
+      </div>
+      <ul className={chatInfoModalBodyStyle}>
+        {isBanListShown
+          ? bannedUsers.map((user, index) => (
+              <BannedUserListElement key={`ban-user-list-${index}`} user={user} currentUserRole={currentUserRole} />
+            ))
+          : users.map((user, index) => (
+              <ChatUserListElement key={`chat-user-list-${index}`} chatUser={user} currentUserRole={currentUserRole} />
+            ))}
+      </ul>
+    </>
   );
 };
