c8376345afcc601655b517fa95c00ae14cf5315a
stripy1026 <inwoo1026@naver.com>
2023-02-22 15:34:27 +0900

    feat: :sparkles: edit each sections to show history info

--- frontend/src/pages/ProfilePage/MatchHistory.tsx
+++ frontend/src/pages/ProfilePage/MatchHistory.tsx
@@ -1,4 +1,5 @@
 import { MatchHistoryType } from 'types/profile';
+import { tmpAvatarStyle } from './tmpAvatarStyle';
 
 interface Props {
   history: MatchHistoryType;
@@ -7,9 +8,15 @@ interface Props {
 export const MatchHistory = ({ history }: Props) => {
   return (
     <div key={history.id}>
-      <div className="Winner">winner</div>
-      <div className="loser">loser</div>
-      <div className="date">date</div>
+      <div className="Winner">
+        <img className={tmpAvatarStyle} src={history.winner.avatar} alt="winnerAvatar" />
+        <span>{history.winner.nickname}</span>
+      </div>
+      <div className="loser">
+        <img className={tmpAvatarStyle} src={history.loser.avatar} alt="winnerAvatar" />
+        <span>{history.loser.nickname}</span>
+      </div>
+      <div className="date">{history.createdAt}</div>
     </div>
   );
 };
