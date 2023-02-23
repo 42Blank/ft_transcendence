e6099632be5e3f82c6d2c351b4cec956becf170a
Kang Jisoon <wltnskjs@naver.com>
2023-02-23 18:44:14 +0900

    feat: :white_check_mark: Game 모달 출력 테스트 성공

--- frontend/src/pages/GamePage/index.tsx
+++ frontend/src/pages/GamePage/index.tsx
@@ -1,18 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
+import { Modal } from 'common';
 import { useGetCurrentGameRoom } from 'hooks';
 import { useSetRecoilState } from 'recoil';
 import { leaveGameRoomState } from 'store';
 import GamePong from './game';
 
+import { GameResultModalBody } from './GameResultModalBody';
+import { newGameModalHeaderStyle } from './GameResultModalBody.styles';
+
 export const GamePage = () => {
+  const [isModalShown] = useState(true);
   const currentGameRoom = useGetCurrentGameRoom();
   const setLeaveGameRoom = useSetRecoilState(leaveGameRoomState);
+
   useEffect(() => {
     return () => {
       setLeaveGameRoom({ id: currentGameRoom.id });
     };
   }, []);
+
+  // function handleClickButton() {
+  //  setIsModalShown(true);
+  // }
 
   return (
     <div>
@@ -20,6 +30,14 @@ export const GamePage = () => {
       {/* 게임 설명 예시 */}
       <span>• BALL WILL SERVE AUTOMATICALLY</span>
       <span>• AVOID MISSING BALL FOR HIGH SCORE</span>
+      {isModalShown && (
+        <Modal onClickClose={() => {}}>
+          <header className={newGameModalHeaderStyle}>
+            <h4> Game Result </h4>
+          </header>
+          <GameResultModalBody />
+        </Modal>
+      )}
     </div>
   );
 };
