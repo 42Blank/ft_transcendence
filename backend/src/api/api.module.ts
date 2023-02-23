03be9fbdee5d57b04b451ecac35f35495ef8d75e
jasong <jasong@student.42seoul.kr>
2023-02-24 01:04:50 +0900

    fix: Match history Module 추가

--- backend/src/api/api.module.ts
+++ backend/src/api/api.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MatchHistory } from 'common/database/entities/match-history.entity';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { FriendModule } from './friend/friend.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, AuthModule, FriendModule],
+  imports: [UserModule, AuthModule, FriendModule, MatchHistory],
 })
 export class ApiModule {}
