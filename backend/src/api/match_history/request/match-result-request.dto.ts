b093653dd495c77df16f96894682f83a93a467c1
jasong <jasong@student.42seoul.kr>
2023-02-24 01:05:34 +0900

    feat: match result dto 구현 완료

---
+++ backend/src/api/match_history/request/match-result-request.dto.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsInt, IsNotEmpty } from 'class-validator';
+import { User } from 'common/database/entities/user.entity';
+
+export class MatchHistoryRequestDto {
+  @IsNotEmpty()
+  @IsInt()
+  @ApiProperty({ example: '1', description: '매치 고유 id' })
+  id: number;
+
+  @IsNotEmpty()
+  @ApiProperty({ description: '승리 유저' })
+  winner: User;
+
+  @IsNotEmpty()
+  @ApiProperty({ description: '패배 유저' })
+  loser: User;
+
+  @IsNotEmpty()
+  @IsDate()
+  @ApiProperty({ description: '경기 시간' })
+  createdAt: Date;
+}
