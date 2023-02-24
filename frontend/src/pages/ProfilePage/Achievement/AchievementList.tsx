478802a5a4cf01bccec46f6f79344a7fb1179cd4
stripy1026 <inwoo1026@naver.com>
2023-02-25 04:33:01 +0900

    feat: :fire: delete userAchivement in useEffect

--- frontend/src/pages/ProfilePage/Achievement/AchievementList.tsx
+++ frontend/src/pages/ProfilePage/Achievement/AchievementList.tsx
@@ -19,7 +19,7 @@ export const AchievementList = ({ userId, className }: Props) => {
       setIsPostDone(true);
       refetch();
     });
-  }, [userId, userAchievement]);
+  }, [userId]);
 
   if (!isPostDone) return <div>loading Achievement List ...</div>;
   return (
