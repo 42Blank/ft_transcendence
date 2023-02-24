dd6d86f561f9c13fb53247005d1c83ed3c7e8642
Kang Jisoon <wltnskjs@naver.com>
2023-02-25 02:23:04 +0900

    feat: :sparkles: useGetMatched 사용하게 결과페이지 수정

--- frontend/src/pages/GameResultPage/index.tsx
+++ frontend/src/pages/GameResultPage/index.tsx
@@ -1,12 +1,12 @@
-import { useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
-import { useRecoilValue } from 'recoil';
-import { finishedGameState } from 'store';
 import { ROUTE } from 'common/constants';
+import { useGetMatchHistoryById } from 'hooks';
 
 export const GameResultPage = () => {
+  const { id } = useParams();
   const nav = useNavigate();
-  const finishedGame = useRecoilValue(finishedGameState);
+  const { data } = useGetMatchHistoryById(Number(id));
 
   function handleClickLinkButton() {
     nav(ROUTE.GAME);
@@ -16,7 +16,7 @@ export const GameResultPage = () => {
     <div>
       <h1>Result Page</h1>
       <h2>
-        🏅{finishedGame.winner.nickname} 🆚 {finishedGame.loser.nickname}
+        🏅{data.winner.nickname} 🆚 {data.loser.nickname}
       </h2>
       <button type="button" onClick={handleClickLinkButton}>
         <span>게임 메뉴로 돌아가기</span>
