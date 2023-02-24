736ea16c4153c22a9055eac88e1b0fc363f3201f
Kang Jisoon <wltnskjs@naver.com>
2023-02-24 20:41:19 +0900

    feat: :seedling: GameResultPage 복귀

---
+++ frontend/src/pages/GameResultPage/index.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
+import { ROUTE } from 'common/constants';
+
+export const GameResultPage = () => {
+  const nav = useNavigate();
+
+  function handleClickLinkButton() {
+    nav(ROUTE.GAME);
+    window.location.reload();
+  }
+  return (
+    <div>
+      <h1>Result Page</h1>
+      <h4>🏅Player VS Player</h4>
+      <h4>5 VS 3</h4>
+      <button type="button" onClick={handleClickLinkButton}>
+        <span>게임 메뉴로 돌아가기</span>
+      </button>
+    </div>
+  );
+};
