634b7dd8d33c7c32b9ecf646373fba0b73e48685
huni <petercha2000@gmail.com>
2023-02-25 05:17:06 +0900

    feat: use .env.prod.for_local

--- .script/deploy.sh
+++ .script/deploy.sh
@@ -14,7 +14,7 @@ docker-compose up --build -d
 
 sed -i 's/8081:/8083:/g' docker-compose.yml
 sed -i 's/5432:/5433:/g' docker-compose.yml
-sed -i 's/FT_CALLBACK_URL=.*/FT_CALLBACK_URL=https:\/\/localhost:3000\/login\/callback/' ./backend/.env
+yes | cp backend/.env.prod.for_local backend/.env
 
 docker-compose -p for_local up -d backend
 
