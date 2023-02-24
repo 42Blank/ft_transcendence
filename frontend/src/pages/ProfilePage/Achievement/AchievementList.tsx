fb4035530252daba72a7d4238703b90f99e86a27
stripy1026 <inwoo1026@naver.com>
2023-02-25 00:50:28 +0900

    feat: :sparkles: add All achiv list and color sign

--- frontend/src/pages/ProfilePage/Achievement/AchievementList.tsx
+++ frontend/src/pages/ProfilePage/Achievement/AchievementList.tsx
@@ -1,3 +1,4 @@
+import { useGetAchievementList } from 'hooks/useGetAchievementList';
 import { useGetUserAchievement } from 'hooks/useGetUserAchievement';
 import { useState } from 'react';
 import { postUserAchievement } from 'services';
@@ -13,14 +14,19 @@ export const AchievementList = ({ userId, className }: Props) => {
   const [isPostDone, setIsPostDone] = useState<Boolean>(false);
   postUserAchievement(userId).then(() => setIsPostDone(true));
   const achievementList: AchievementType[] = useGetUserAchievement(userId).userAchievement;
+  const allAchievementList: AchievementType[] = useGetAchievementList().achievementList;
 
   if (!isPostDone) return <div>loading Achievement List ...</div>;
   return (
     <div className={className}>
       <h1>Achievement</h1>
       <h2> debug: ID : {userId}</h2>
-      {achievementList.map((value: AchievementType) => (
-        <Achievement key={value.id} achieve={value} />
+      {allAchievementList.map((value: AchievementType) => (
+        <Achievement
+          key={value.id}
+          achieve={value}
+          isAchieved={achievementList.some(active => active.id === value.id)}
+        />
       ))}
     </div>
   );
