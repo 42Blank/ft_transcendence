aec23189984f20dc4b407e080b83bf7096200633
huni <petercha2000@gmail.com>
2023-02-25 06:56:09 +0900

    feat: file done

--- backend/src/api/file/file.controller.ts
+++ backend/src/api/file/file.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Param, Post, Res, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiBody, ApiConsumes, ApiCookieAuth, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { Response } from 'express';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
 import { UserJwtAuthGuard } from '../../common/auth/jwt-auth';
 
 @ApiTags('File')
@@ -10,7 +13,26 @@ import { UserJwtAuthGuard } from '../../common/auth/jwt-auth';
 @ApiUnauthorizedResponse({ description: '로그인이 필요합니다.' })
 export class FileController {
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+      fileFilter: (req, file, callback) => {
+        if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Only image files are allowed!'), false);
+        }
+      },
+    }),
+  )
   @ApiConsumes('multipart/form-data')
   @ApiBody({
     schema: {
@@ -24,6 +46,14 @@ export class FileController {
     },
   })
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+    return file.filename;
+  }
+
+  @Get(':filename')
+  downloadFile(
+    @Param('filename') filename: string, //
+    @Res() res: Response,
+  ) {
+    res.sendFile(join(process.cwd(), 'uploads', filename));
   }
 }
