9e8ea688d32c45a10f035d7e204edbb478d16a50
jasong <jasong@student.42seoul.kr>
2023-02-24 18:42:26 +0900

    fix: 업적 update 로직 변경

--- backend/src/api/achievement/services/update-user-achievement.service.ts
+++ backend/src/api/achievement/services/update-user-achievement.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Achievement } from 'common/database/entities/achievement.entity';
 import { Friend } from 'common/database/entities/friend.entity';
 import { MatchHistory } from 'common/database/entities/match-history.entity';
 import { UserAchievement } from 'common/database/entities/user-achievement.entity';
@@ -8,6 +9,8 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class UpdateUserAchievementService {
   constructor(
+    @InjectRepository(Achievement)
+    private readonly achievementRepository: Repository<Achievement>,
     @InjectRepository(UserAchievement)
     private readonly userAchievementRepository: Repository<UserAchievement>,
     @InjectRepository(MatchHistory)
@@ -16,98 +19,94 @@ export class UpdateUserAchievementService {
     private readonly friendRepository: Repository<Friend>,
   ) {}
 
-  async updateAchievement(userId: number): Promise<void> {
-    const userAchievements = await this.userAchievementRepository.find({
-      where: {
-        userId: userId,
-      },
-      relations: ['achievement'],
-    });
+  private getMaxStreak(matchHistories: MatchHistory[], userId: number): number {
+    let maxStreak = 0;
+    matchHistories.reduce((acc, cur) => {
+      if (cur.winnerId === userId) {
+        if (maxStreak < acc + 1) maxStreak = acc + 1;
+        return acc + 1;
+      } else {
+        return 0;
+      }
+    }, 0);
+    return maxStreak;
+  }
 
+  private getMinStreak(matchHistories: MatchHistory[], userId: number): number {
+    let minStreak = 0;
+    matchHistories.reduce((acc, cur) => {
+      if (cur.loserId === userId) {
+        if (minStreak < acc + 1) minStreak = acc + 1;
+        return acc + 1;
+      } else {
+        return 0;
+      }
+    }, 0);
+    return minStreak;
+  }
+
+  private isAchieved(
+    achievement: Achievement,
+    winCount: number,
+    loseCount: number,
+    winStreak: number,
+    loseStreak: number,
+    friendCount: number,
+  ): boolean {
+    if (achievement.winCount && achievement.winCount > winCount) return false;
+    if (achievement.loseCount && achievement.loseCount > loseCount) return false;
+    if (achievement.winStreak && achievement.winStreak > winStreak) return false;
+    if (achievement.loseStreak && achievement.loseStreak > loseStreak) return false;
+    if (achievement.friendCount && achievement.friendCount > friendCount) return false;
+    return true;
+  }
+
+  async updateAchievement(userId: number): Promise<void> {
     const matchHistories = await this.matchHistoryRepository.find({
       where: [{ winnerId: userId }, { loserId: userId }],
     });
 
-    const totalWin = matchHistories.filter(matchHistory => matchHistory.winnerId === userId);
-    const totalLose = matchHistories.filter(matchHistory => matchHistory.loserId === userId);
-
-    const winStreak = matchHistories.reduce((acc, cur) => {
-      if (cur.winnerId === userId) {
-        return acc + 1;
-      } else {
-        return 0;
-      }
-    }, 0);
-    const loseStreak = matchHistories.reduce((acc, cur) => {
-      if (cur.loserId === userId) {
-        return acc + 1;
-      } else {
-        return 0;
-      }
-    }, 0);
-
-    const firstWinAchievement = userAchievements.find(userAchievement => userAchievement.achievement.id === 1);
-    if (!firstWinAchievement && totalWin.length >= 1) {
-      await this.userAchievementRepository.save({
-        userId: userId,
-        achievementId: 1,
-      });
-    }
-    const firstThreeWinAchievement = userAchievements.find(userAchievement => userAchievement.achievement.id === 2);
-    if (!firstThreeWinAchievement && totalWin.length >= 3) {
-      await this.userAchievementRepository.save({
-        userId: userId,
-        achievementId: 2,
-      });
-    }
-    const winStreakThreeAchievement = userAchievements.find(userAchievement => userAchievement.achievement.id === 3);
-    if (!winStreakThreeAchievement && winStreak >= 3) {
-      await this.userAchievementRepository.save({
-        userId: userId,
-        achievementId: 3,
-      });
-    }
-    const firstLoseAchievement = userAchievements.find(userAchievement => userAchievement.achievement.id === 4);
-    if (!firstLoseAchievement && totalLose.length >= 1) {
-      await this.userAchievementRepository.save({
-        userId: userId,
-        achievementId: 4,
-      });
-    }
-    const firstThreeLoseAchievement = userAchievements.find(userAchievement => userAchievement.achievement.id === 5);
-    if (!firstThreeLoseAchievement && totalLose.length >= 3) {
-      await this.userAchievementRepository.save({
-        userId: userId,
-        achievementId: 5,
-      });
-    }
-    const loseStreakThreeAchievement = userAchievements.find(userAchievement => userAchievement.achievement.id === 6);
-    if (!loseStreakThreeAchievement && loseStreak >= 3) {
-      await this.userAchievementRepository.save({
-        userId: userId,
-        achievementId: 6,
-      });
-    }
-    const firstAddFriendAchievement = userAchievements.find(userAchievement => userAchievement.achievement.id === 7);
+    const winCount = matchHistories.filter(matchHistory => matchHistory.winnerId === userId).length;
+    const loseCount = matchHistories.filter(matchHistory => matchHistory.loserId === userId).length;
+    const winStreak = this.getMaxStreak(matchHistories, userId);
+    const loseStreak = this.getMinStreak(matchHistories, userId);
     const friends = await this.friendRepository.find({
       where: {
         sendFriendRequestUserId: userId,
       },
     });
-    if (!firstAddFriendAchievement && friends.length >= 1) {
-      await this.userAchievementRepository.save({
-        userId: userId,
-        achievementId: 7,
-      });
+    const friendCount = friends.length;
+    const achievements = await this.achievementRepository.find();
+
+    for (const achievement of achievements) {
+      if (this.isAchieved(achievement, winCount, loseCount, winStreak, loseStreak, friendCount)) {
+        await this.userAchievementRepository.upsert(
+          {
+            userId: userId,
+            achievementId: achievement.id,
+          },
+          {
+            conflictPaths: ['userId', 'achievementId'],
+          },
+        );
+      }
     }
-    const firstThreeAddFriendAchievement = userAchievements.find(
-      userAchievement => userAchievement.achievement.id === 8,
-    );
-    if (!firstThreeAddFriendAchievement && friends.length >= 3) {
-      await this.userAchievementRepository.save({
-        userId: userId,
-        achievementId: 8,
-      });
-    }
+
+    // TODO: SEE AGAIN
+    // await Promise.all(
+    //   achievements.map(async achievement => {
+    //     if (this.isAchieved(achievement, winCount, loseCount, winStreak, loseStreak, friendCount)) {
+    //       await this.userAchievementRepository.upsert(
+    //         {
+    //           userId: userId,
+    //           achievementId: achievement.id,
+    //         },
+    //         {
+    //           conflictPaths: ['userId', 'achievementId'],
+    //         },
+    //       );
+    //     }
+    //   }),
+    // );
   }
 }
