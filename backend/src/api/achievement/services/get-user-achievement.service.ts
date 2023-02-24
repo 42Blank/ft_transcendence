1b02f52dc0b789c967474f0af96605b91a5f70ea
jasong <jasong@student.42seoul.kr>
2023-02-24 18:41:01 +0900

    fix: 파일 이름 변경

--- backend/src/api/achievement/services/get-user-achievement.service.ts
+++ backend/src/api/achievement/services/get-user-achievement.service.ts
@@ -1,35 +1,24 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Achievement } from 'common/database/entities/achievement.entity';
 import { UserAchievement } from 'common/database/entities/user-achievement.entity';
-import { User } from 'common/database/entities/user.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
-export class GetAchievementService {
+export class GetUserAchievementService {
   constructor(
     @InjectRepository(UserAchievement)
     private readonly userAchievementRepository: Repository<UserAchievement>,
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
   ) {}
 
-  async getAchievementById(userId: number): Promise<Achievement[]> {
-    // 여기서도 user not found를 체크해주는게 맞는지??
-    const user = this.userRepository.findOne({
-      where: {
-        id: userId,
-      },
-    });
-
-    if (!user) throw new NotFoundException(`User with id ${userId} not found`);
-
+  async getUserAchievementById(userId: number): Promise<Achievement[]> {
     const userAchievements = await this.userAchievementRepository.find({
       where: {
         userId: userId,
       },
       relations: ['achievement'],
     });
+    console.log(userAchievements);
     return userAchievements.map(userAchievement => userAchievement.achievement);
   }
 }
