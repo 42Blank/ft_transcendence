d07b9e5fff0a533063d8920b883327e9f057f54b
stripy1026 <inwoo1026@naver.com>
2023-02-24 23:44:25 +0900

    feat: :sparkles: add initial userAchiv

--- frontend/src/hooks/useGetUserAchievement.ts
+++ frontend/src/hooks/useGetUserAchievement.ts
@@ -3,12 +3,16 @@ import { useQuery } from 'react-query';
 import { getUserAchievement } from 'services';
 
 export function useGetUserAchievement(userId: number) {
-  const { data: userAchievement, refetch } = useQuery([`achievement_${userId}`], () => getUserAchievement(userId), {
-    refetchOnWindowFocus: false,
-    staleTime: 1000 * 60,
-    cacheTime: 1000 * 60,
-    retry: 0,
-    useErrorBoundary: true,
-  });
+  const { data: userAchievement = [], refetch } = useQuery(
+    [`achievement_${userId}`],
+    () => getUserAchievement(userId),
+    {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60,
+      cacheTime: 1000 * 60,
+      retry: 0,
+      useErrorBoundary: true,
+    },
+  );
   return { userAchievement, refetch };
 }
