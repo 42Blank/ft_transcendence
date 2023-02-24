552ad8310eac316469f69ef3a40921fd41dc7733
stripy1026 <inwoo1026@naver.com>
2023-02-24 16:40:41 +0900

    feat: :sparkles: MatchHistory box component done

--- frontend/src/pages/ProfilePage/MatchHistory/MatchHistoryBox.tsx
+++ frontend/src/pages/ProfilePage/MatchHistory/MatchHistoryBox.tsx
@@ -1,3 +1,18 @@
-export const MatchHistoryBox = (key: string) => {
-  return <div></div>;
+import { UserInfoType } from 'types/user';
+
+import { matchAvatarStyle, matchHistoryBoxStyle } from './MatchHistory.style';
+
+interface Props {
+  user: UserInfoType;
+  key: 'WIN' | 'LOSE';
+}
+
+export const MatchHistoryBox = ({ user, key }: Props) => {
+  return (
+    <div className={matchHistoryBoxStyle}>
+      <img className={matchAvatarStyle} src={user.avatar} width={100} height={100} alt={`${key}Avatar`} />
+      <span className="win-lose">{key}</span>
+      <span className="nick">{user.nickname}</span>
+    </div>
+  );
 };
