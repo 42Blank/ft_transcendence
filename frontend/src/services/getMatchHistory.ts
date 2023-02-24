809cecfe31a65eace5d690506eb110b8093c77ff
Kang Jisoon <wltnskjs@naver.com>
2023-02-25 02:06:09 +0900

    chore: :truck: getMatchHistoryByUserId.ts로 파일명 변경

--- frontend/src/services/getMatchHistory.ts
+++
@@ -1,7 +0,0 @@
-import { API } from 'common/constants';
-import { MatchHistoryType } from 'types/profile';
-import { axiosGet } from './axiosWrapper';
-
-export async function getMatchHistory(userId: number): Promise<MatchHistoryType[]> {
-  return axiosGet<MatchHistoryType[]>(`${API.MATCH}/${userId}`);
-}
