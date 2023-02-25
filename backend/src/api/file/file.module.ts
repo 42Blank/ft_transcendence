1f98750fd837992c9fae28221567812e70a2872e
huni <petercha2000@gmail.com>
2023-02-26 03:47:39 +0900

    feat: 안쓰는거 삭제

--- backend/src/api/file/file.module.ts
+++ backend/src/api/file/file.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { MulterModule } from '@nestjs/platform-express';
 import { FileController } from './file.controller';
 
 @Module({
-  imports: [
-    MulterModule.register({
-      dest: './upload',
-    }),
-  ],
   controllers: [FileController],
 })
 export class FileModule {}
