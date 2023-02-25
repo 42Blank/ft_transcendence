16de4c06083948ffa44fe6f080f2387a189a16b3
Kang Jisoon <wltnskjs@naver.com>
2023-02-26 04:34:09 +0900

    feat: :sparkles: 게임 생성 모달 드롭다운 추가

--- frontend/src/pages/GameListPage/NewGameModalBody.tsx
+++ frontend/src/pages/GameListPage/NewGameModalBody.tsx
@@ -1,7 +1,9 @@
-import { FormEvent } from 'react';
+import { Dropdown } from 'common';
+import { FormEvent, useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { newGameRoomState } from 'store';
 import { playerRoleState } from 'store/playerRoleState';
+// import { NewGameRoomType } from 'types/game';
 
 import {
   formSectionButtonWrapper,
@@ -15,15 +17,29 @@ interface Props {
 }
 
 export const NewGameModalBody = ({ onClickClose }: Props) => {
+  const [isNormal, setIsNormal] = useState(true);
+  // const [gameMode, setGameMode] = useState<NewGameRoomType['mode']>('normal');
   const setNewGameRoom = useSetRecoilState(newGameRoomState);
   const setPlayerRole = useSetRecoilState(playerRoleState);
 
+  const dropdownElement = [
+    { key: '일반', value: true },
+    { key: 'RED', value: false },
+  ];
+
+  function handleToggleMode(value: number | boolean) {
+    setIsNormal(value as boolean);
+    // setGameMode(isNormal ? 'normal' : 'red');
+    // console.log('1.MODE: ' + gameMode);
+  }
   function handleOnClick(e: FormEvent) {
     // roomTitle 필요없어서 처내야함.
     e.preventDefault();
     setPlayerRole({ role: 'host' });
+    const mode = isNormal ? 'normal' : 'red';
     setNewGameRoom({
       created: true,
+      mode,
     });
     onClickClose();
   }
@@ -32,8 +48,8 @@ export const NewGameModalBody = ({ onClickClose }: Props) => {
     <div className={newGameFormStyle}>
       <div className={newGameInnerDivStyle}>
         <div className={formSectionDivStyle}>
-          <input type="checkbox" disabled={} />
-          <label>RED MODE ON</label>
+          <span>모드 설정</span>
+          <Dropdown currentKey={isNormal ? '일반' : 'RED'} elements={dropdownElement} onChange={handleToggleMode} />
         </div>
       </div>
       <div className={formSectionButtonWrapper}>
