c9747db347f38ff647c7ba7341f83119ac293530
jasong <jasong@student.42seoul.kr>
2023-02-25 22:44:02 +0900

    fix: 업적 세부정보 수정 및 이미지 url추가

--- backend/src/api/achievement/achievement.module.ts
+++ backend/src/api/achievement/achievement.module.ts
@@ -26,33 +26,33 @@ export class AchievementModule implements OnModuleInit {
     // 상수처럼 취급하려면 Readonly 와 as const를 붙혀서 사용할 수 있다. const가 reference를 고정시키는 것. as const는 literal type을 고정시키는 것. 배열을 고정시키는 것이 아니다.
     const achievements: Omit<Achievement, 'id' | 'userAchievement'>[] = [
       {
-        name: '첫 승리',
-        description: '첫 승리를 기록하세요',
-        image: 'test',
+        name: '내가 첫 승리라니!',
+        description: '전체 게임에서 첫 승리 달성',
+        image: 'https://x.boardgamearena.net/data/themereleases/230221-1000//img/awards/1.png',
         winCount: 1,
       },
       {
-        name: '첫 패배',
-        description: '첫 패배를 기록하세요',
-        image: 'test',
+        name: '패배의 쓴맛',
+        description: '전체 게임에서 첫 패배 달성',
+        image: 'https://x.boardgamearena.net/data/themereleases/230221-1000//img/awards/146.png',
         loseCount: 1,
       },
       {
-        name: '첫 친구',
-        description: '첫 친구를 만드세요',
-        image: 'test',
+        name: '동반자',
+        description: '친구 목록에 1명 이상의 친구 등록',
+        image: 'https://x.boardgamearena.net/data/themereleases/230221-1000//img/awards/72.png',
         friendCount: 1,
       },
       {
-        name: '3연승',
-        description: '3연승을 기록하세요',
-        image: 'test',
+        name: '연승가도!',
+        description: '전체 게임에서 3연승 달성',
+        image: 'https://x.boardgamearena.net/data/themereleases/230221-1000//img/awards/2.png',
         winCount: 3,
       },
       {
-        name: '3연패',
-        description: '3연패를 기록하세요',
-        image: 'test',
+        name: '어... 이게 아닌데...',
+        description: '전체 게임에서 3연패 달성',
+        image: 'https://x.boardgamearena.net/data/themereleases/230221-1000//img/awards/148.png',
         loseCount: 3,
       },
     ];
