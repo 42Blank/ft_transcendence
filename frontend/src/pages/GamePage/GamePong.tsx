7f19b6f371b16e32189a5914b88ee0a815ee62db
Kang Jisoon <wltnskjs@naver.com>
2023-02-26 00:21:15 +0900

    fix: :bug: 게임 비정상 재접속 후 Phaser 에러 수정

--- frontend/src/pages/GamePage/GamePong.tsx
+++ frontend/src/pages/GamePage/GamePong.tsx
@@ -3,31 +3,48 @@ import { useNavigate } from 'react-router-dom';
 
 import Phaser from 'phaser';
 import { GameInstance, IonPhaser } from '@ion-phaser/react';
+import { sockets } from 'hooks';
 
 import { useRecoilValue } from 'recoil';
 import { playerRoleState } from 'store/playerRoleState';
 import { MainScene } from './MainScene';
 
-const mainScene = new MainScene();
-
-const game: GameInstance = {
+const gameConfig: GameInstance = {
   width: 800,
   height: 600,
   type: Phaser.AUTO,
-  scene: mainScene,
   physics: { default: 'arcade', arcade: { gravity: { y: 0 }, fps: 60 } },
 };
 
 const GamePong = () => {
+  const [game, setGame] = useState<GameInstance>();
+  const [initialize, setInitialize] = useState(true);
   const gameRef = useRef<HTMLIonPhaserElement>(null);
-  const [initialize] = useState(true);
   const playerRole = useRecoilValue(playerRoleState);
   const nav = useNavigate();
 
+  const destroy = () => {
+    gameRef.current?.destroy();
+    setInitialize(false);
+    setGame(null);
+  };
+
   useEffect(() => {
+    const mainScene = new MainScene();
+    console.log('SetGame!');
+
+    setGame({ ...gameConfig, scene: mainScene });
     mainScene.initHandlers();
     mainScene.hostCheckHandlers(playerRole.role);
     mainScene.naviHandlers(nav);
+    // mainScene.initGame();
+
+    return () => {
+      console.log('destroy!!');
+      sockets.gameSocket.removeListener('game_data');
+      sockets.gameSocket.removeListener('update_score');
+      destroy();
+    };
   }, []);
 
   return <IonPhaser ref={gameRef} game={game} initialize={initialize} />;
