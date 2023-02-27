15fef121b4d62b3ec7de1d33c223cdd6b4d4b0bc
Kang Jisoon <wltnskjs@naver.com>
2023-02-27 17:25:14 +0900

    feat: :lipstick: 게임리스트에 랭크 아이콘 추가

--- frontend/src/pages/GameListPage/index.tsx
+++ frontend/src/pages/GameListPage/index.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
-import { PlusIcon } from 'assets';
+import { FightIcon, PlusIcon } from 'assets';
 import { Modal } from 'common';
 import { gameRoomListState } from 'store';
 import { GameRoomElement } from './GameRoomElement';
 
 import {
   gameListWrapperStyle,
+  gameMatchIconStyle,
   gameRoomIconStyle,
   newGameModalHeaderStyle,
   newGameModalWrapperStyle,
@@ -17,6 +18,10 @@ import { NewGameModalBody } from './NewGameModalBody';
 export const GameListPage = () => {
   const [isModalShown, setIsModalShown] = useState(false);
   const gameRoomList = useRecoilValue(gameRoomListState);
+
+  // joinmatchmake
+  // leavematchmake
+  // 변경점이 있어야 recoil 업데이트 되니까 아무거나 넣으면 됨.
 
   function handleClickButton() {
     setIsModalShown(true);
@@ -32,6 +37,9 @@ export const GameListPage = () => {
       ))}
       <button type="button" onClick={handleClickButton} className={gameRoomIconStyle}>
         <PlusIcon />
+      </button>
+      <button type="button" onClick={handleClickButton} className={gameMatchIconStyle}>
+        <FightIcon />
       </button>
       {isModalShown && (
         <Modal onClickClose={handleClickClose} className={newGameModalWrapperStyle}>
