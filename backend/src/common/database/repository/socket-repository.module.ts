a09afcc50489b8cc53d49391b7823fe26a4f80db
jasong <jasong@student.42seoul.kr>
2023-03-01 04:07:58 +0900

    fix: socket module에 MatchQueue 추가

--- backend/src/common/database/repository/socket-repository.module.ts
+++ backend/src/common/database/repository/socket-repository.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChatRoomRepository } from './chat-room.repository';
+import { GameMatchQueueRepository } from './game-match-queue.repository';
 import { GameRoomRepository } from './game-room.repository';
 import { OnlineUserRepository } from './online-user.repository';
 
 @Module({
-  providers: [ChatRoomRepository, GameRoomRepository, OnlineUserRepository],
-  exports: [ChatRoomRepository, GameRoomRepository, OnlineUserRepository],
+  providers: [ChatRoomRepository, GameRoomRepository, OnlineUserRepository, GameMatchQueueRepository],
+  exports: [ChatRoomRepository, GameRoomRepository, OnlineUserRepository, GameMatchQueueRepository],
 })
 export class SocketRepositoryModule {}
