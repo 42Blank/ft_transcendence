ef59ea79702a0fef6867283404870485514f7d91
Kang Jisoon <wltnskjs@naver.com>
2023-03-01 16:59:15 +0900

    feat: :sparkles: 2MB 이하 파일만 등록 가능 기능 추가

--- frontend/src/common/ChangeProfiles/ChangeAvatar.tsx
+++ frontend/src/common/ChangeProfiles/ChangeAvatar.tsx
@@ -18,6 +18,16 @@ interface Props {
 
 export const ChangeAvatar = ({ imageUrl, onChange, className }: Props) => {
   function handleChangeImage(e: ChangeEvent<HTMLInputElement>) {
+    if (e.currentTarget.files[0].size > 1024 * 1024 * 2) {
+      alert(
+        '2MB 이하 파일만 등록할 수 있습니다.\n\n' +
+          '현재파일 용량 : ' +
+          Math.round((e.currentTarget.files[0].size / 1024 / 1024) * 100) / 100 +
+          'MB',
+      );
+      return;
+    }
+
     onChange(LOADING_IMAGE_URL);
     postFile({ file: e.currentTarget.files[0] }).then(res => {
       onChange(`${process.env.REACT_APP_SERVER}/file/${res}`);
