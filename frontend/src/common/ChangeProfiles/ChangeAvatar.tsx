fb52e0612d12079cb832b4824e85fbc4840688b6
Kang Jisoon <wltnskjs@naver.com>
2023-03-01 17:15:07 +0900

    fix: :adhesive_bandage: fix es-lint

--- frontend/src/common/ChangeProfiles/ChangeAvatar.tsx
+++ frontend/src/common/ChangeProfiles/ChangeAvatar.tsx
@@ -20,10 +20,9 @@ export const ChangeAvatar = ({ imageUrl, onChange, className }: Props) => {
   function handleChangeImage(e: ChangeEvent<HTMLInputElement>) {
     if (e.currentTarget.files[0].size > 1024 * 1024 * 2) {
       alert(
-        '2MB 이하 파일만 등록할 수 있습니다.\n\n' +
-          '현재파일 용량 : ' +
-          Math.round((e.currentTarget.files[0].size / 1024 / 1024) * 100) / 100 +
-          'MB',
+        `2MB 이하 파일만 등록할 수 있습니다.\n\n현재파일 용량 : ${
+          Math.round((e.currentTarget.files[0].size / 1024 / 1024) * 100) / 100
+        }MB`,
       );
       return;
     }
