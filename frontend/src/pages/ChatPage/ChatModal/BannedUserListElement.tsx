85cd38637d9a3d4018743f54893b9384410addc3
chichoon <chichoon.choi@gmail.com>
2023-03-02 12:35:17 +0900

    style: :lipstick: 밴당한 유저 ui

--- frontend/src/pages/ChatPage/ChatModal/BannedUserListElement.tsx
+++ frontend/src/pages/ChatPage/ChatModal/BannedUserListElement.tsx
@@ -1,14 +1,17 @@
-import { useSetRecoilState } from 'recoil';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { UnbanIcon } from 'assets';
+import { ArrowDownIcon } from 'assets';
+import { Avatar, Button } from 'common';
 import { ROUTE } from 'common/constants';
-import { userOperationState } from 'store';
 import { ChatUserRoleType } from 'types/chat';
 import { UserInfoType } from 'types/user';
+import { COMMON_SIZES } from 'styles';
+import { BanUserOperationBox } from './banUserOperationBox';
 
 import {
-  chatUserButtonStyle,
+  chatUserDrawerButtonStyle,
+  chatUserDrawerStyle,
   chatUserElementImageStyle,
   chatUserElementWrapperStyle,
   chatUserLinkWrapperStyle,
@@ -21,29 +24,33 @@ interface Props {
 }
 
 export const BannedUserListElement = ({ user, currentUserRole }: Props) => {
-  const setOperation = useSetRecoilState(userOperationState);
+  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
 
-  function handleClickUnbanIcon() {
-    setOperation({ userId: user.id, operation: 'unban' });
+  function handleClickToggleDrawer() {
+    setIsDrawerOpen(prevState => !prevState);
   }
 
   return (
-    <li className={chatUserElementWrapperStyle}>
-      <Link to={`${ROUTE.PROFILE}/${user.id}`} className={chatUserLinkWrapperStyle('user')}>
-        <img
-          src={user.avatar}
-          alt={`${user.nickname}-avatar`}
-          width={50}
-          height={50}
-          className={chatUserElementImageStyle}
-        />
-        <span className={chatUserNicknameSpanStyle}>{user.nickname}</span>
-      </Link>
-      {currentUserRole !== 'user' && (
-        <button type="button" onClick={handleClickUnbanIcon} className={chatUserButtonStyle}>
-          <UnbanIcon />
-        </button>
-      )}
-    </li>
+    <>
+      <li className={chatUserElementWrapperStyle}>
+        <Link to={`${ROUTE.PROFILE}/${user.id}`} className={chatUserLinkWrapperStyle('user')}>
+          <Avatar
+            userAvatar={user.avatar}
+            alt={`${user.nickname}-avatar`}
+            size={COMMON_SIZES.ICON_XLARGE}
+            className={chatUserElementImageStyle}
+          />
+          <span className={chatUserNicknameSpanStyle}>{user.nickname}</span>
+        </Link>
+        {currentUserRole !== 'user' && (
+          <Button onClick={handleClickToggleDrawer} className={chatUserDrawerButtonStyle(isDrawerOpen)}>
+            <ArrowDownIcon />
+          </Button>
+        )}
+      </li>
+      <div className={chatUserDrawerStyle(isDrawerOpen)}>
+        <BanUserOperationBox banUser={user} currentUserRole={currentUserRole} />
+      </div>
+    </>
   );
 };
